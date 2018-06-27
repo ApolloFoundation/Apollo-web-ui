55e00cf571a3ad4a66514e8236eed0b4b896101b
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@s4b.team>
2018-06-27 12:54:13 +0300

    Added Project Template

---
+++ src/js/functions.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+module.exports = {
+	/*
+	 * Functions to be exported go here. Format:
+	 * exportName: functionName
+	 */
+	
+};
+
---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var functions = require('./functions');
+
+/*
+ * Exported in 'functions.js' functions can be used here:
+ * functions.exportName()
+ */
+
+(function () {
+
+	// Code goes here
+	
+})();
