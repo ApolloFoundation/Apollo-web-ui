4903a094ee5a93758b9e9132bc05f73adaeb0cb9
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-04 02:57:40 -0400

    Added modal window component

--- src/js/index.js
+++ src/js/index.js
@@ -10,5 +10,27 @@ var functions = require('./functions');
 (function () {
 
 	// Code goes here
-	
+    var openModalBtn  = document.querySelector('[data-modal="sendMoney"]');
+    var closeModalBtn = document.querySelector('[data-close-modal]');
+    var modalWindow   = document.querySelector('.modal-window');
+
+    openModalBtn .addEventListener('click', openModal);
+    closeModalBtn.addEventListener('click', closeModal);
+    modalWindow  .addEventListener('click', modalController);
+
+    function openModal() {
+        modalWindow.classList.add('active')
+    }
+
+    function closeModal(e) {
+        modalWindow.classList.remove('active');
+    }
+
+    function modalController(e) {
+        if (!e.target.closest('.modal-window .modal-box')) {
+            closeModal();
+        }
+    }
+
+
 })();
