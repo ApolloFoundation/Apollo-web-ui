c23165afad10d186e67aba988bc9d90b9c35a171
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-11 19:18:02 +0300

    Added site header searchbar animation

--- src/containers/components/site-header/index.js
+++ src/containers/components/site-header/index.js
@@ -1,13 +1,41 @@
 import React from "react";
 import './SiteHeader.css';
+import classNames from 'classnames';
 
 class SiteHeader extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            searching: false
+        };
+
+        this.setSearchStateToActive   = this.setSearchStateToActive.bind(this);
+        this.resetSearchStateToActive = this.resetSearchStateToActive.bind(this);
+        this.searchInterval;
+    }
+
+    setSearchStateToActive() {
+        clearInterval(this.searchInterval);
+        this.setState({
+            ...this.state,
+            searching: true
+        });
+
+        console.log(this.state.searching);
+    }
+
+    resetSearchStateToActive() {
+        this.searchInterval = setTimeout(() => {
+            this.setState({
+                ...this.state,
+                searching: false
+            });
+        }, 2000);
     }
 
     render() {
-        console.log(this.props);
+
         return (
             <div className="page-header">
                 <div className="container-fluid">
@@ -32,8 +60,12 @@ class SiteHeader extends React.Component {
                         </div>
                         <div className="col-md-6">
                             <div className="user-search-box">
-                                <div className="search-bar">
-                                    <input type="text"/>
+                                {/*TODO : fix site header search animation*/}
+                                <div onMouseOut={this.resetSearchStateToActive} className={classNames({
+                                    'search-bar' : true,
+                                    'searching' : this.state.searching
+                                })}>
+                                    <input type="text" onMouseOver={this.setSearchStateToActive}/>
                                     <div className="user-account-actions">
                                         <a className="user-account-rs">
                                             APL-P7FK-7FAQ-U632-E2EN6
@@ -41,7 +73,7 @@ class SiteHeader extends React.Component {
                                         <a className="user-account-action"><i className="zmdi zmdi-balance-wallet"></i></a>
                                         <a className="user-account-action"><i className="zmdi zmdi-settings"></i></a>
                                         <a className="user-account-action"><i className="zmdi zmdi-help"></i></a>
-                                        <a className="user-account-action"><i className="zmdi zmdi-search"></i></a>
+                                        <a className="user-account-action" onClick={this.setSearchStateToActive}><i className="zmdi zmdi-search"></i></a>
                                     </div>
                                 </div>
                                 <div className="user-box">
