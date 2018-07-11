6a5a3dc4b24d191bd62a53244eb01a3c066bce13
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-11 12:28:55 +0300

    Added open orders as react component

---
+++ src/containers/account/open-orders/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SiteHeader from '../../components/site-header'
+
+class OpenOrders extends React.Component {
+    render () {
+        return (
+            <div className="page-content">
+                <SiteHeader
+                    pageTitle={'Asset exchange'}
+                />
+                <div className="page-body container-fluid">
+                    <div className="open-orders">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="approval-request white-space">
+                                    <div className="alert">No assets.</div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="approval-request white-space">
+                                    <div className="alert">No assets.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default OpenOrders;
