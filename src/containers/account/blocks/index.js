6dbccf9fa55ee55beb9d876e377105cc32d2398c
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-11 12:12:48 +0300

    Added blocks component as react component

---
+++ src/containers/account/blocks/index.js
@@ -0,0 +1,309 @@
+import React from 'react';
+import SiteHeader from '../../components/site-header'
+
+class Blocks extends React.Component {
+    render () {
+        return (
+            <div className="page-content">
+                <SiteHeader
+                    pageTitle={'Blocks'}
+                />
+                <div className="page-body container-fluid">
+                    <div className="blocks">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <div className="card header ballance single">
+                                    <div className="card-title">Available Balance</div>
+                                    <div className="amount">37,000,000</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <div className="card header assets single">
+                                    <div className="card-title">Available Balance</div>
+                                    <div className="amount">37,000,000</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <div className="card header currencies single">
+                                    <div className="card-title">Available Balance</div>
+                                    <div className="amount">37,000,000</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3">
+                                <div className="card header coins single">
+                                    <div className="card-title">Available Balance</div>
+                                    <div className="amount">37,000,000</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="transaction-table">
+                            <div className="transaction-table-body">
+                                <table>
+                                    <thead>
+                                    <tr>
+                                        <td>Height</td>
+                                        <td className="align-right">Date</td>
+                                        <td className="align-right">Amount</td>
+                                        <td className="align-right">Fee</td>
+                                        <td className="align-right"># TX</td>
+                                        <td>Generator</td>
+                                        <td className="align-right">Payload</td>
+                                        <td className="align-right">Base Target</td>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="blue-link-text"><a>145 804</a>
+                                        </td>
+                                        <td className="align-right"><a>6/13/2018 11:34:03</a>
+                                        </td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right">0</td>
+                                        <td className="align-right"><a>1</a>
+                                        </td>
+                                        <td className="blue-link-text"><a>APL-N6J9-T9AA-RKEB-G9NDM</a>
+                                        </td>
+                                        <td className="align-right"><a>216 B</a>
+                                        </td>
+                                        <td className="align-right"><a>4281 %</a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <div className="btn-box"><a className="btn btn-left"> Previous</a><a
+                                    className="btn btn-right">Next</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Blocks;
