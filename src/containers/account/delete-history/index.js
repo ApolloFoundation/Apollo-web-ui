c0374c4566bffd36796eff2cac8144bcfc8edd4a
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-11 12:15:03 +0300

    Added delete history as react component

---
+++ src/containers/account/delete-history/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SiteHeader from '../../components/site-header'
+
+class DeleteHistory extends React.Component {
+    render () {
+        return (
+            <div className="page-content">
+                <SiteHeader
+                    pageTitle={'Asset exchange'}
+                />
+                <div className="page-body container-fluid">
+                    <div className="scheduled-transactions">
+                        <div className="approval-request white-space">
+                            <div className="alert">No delete history available.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DeleteHistory;
