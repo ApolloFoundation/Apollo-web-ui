ccfdb2d1b8789d5b8b868d0d9fdb7ad0f5ac4bb7
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-23 18:11:35 +0300

    Fixed config, attempt 2

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'https://51.15.235.41:6876/apl?',
+    serverUrl: 'https://apolloface-reactdev.firstbridge.work:6876/apl?',
 };
 
 module.exports = config;
