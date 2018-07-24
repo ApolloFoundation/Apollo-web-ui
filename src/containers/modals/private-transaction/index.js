064d982032fee3c5dccb7109c262dc22fb4c08ef
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-07-24 13:35:14 +0300

    Migrated show private transactions to react-form

--- src/containers/modals/private-transaction/index.js
+++ src/containers/modals/private-transaction/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Form, Text, Radio, RadioGroup, TextArea, Checkbox } from "react-form";
 import {connect} from 'react-redux';
 import {setModalData} from '../../../modules/modals';
+import curve25519 from '../../../helpers/crypto/curve25519'
 
 class PrivateTransactions extends React.Component {
     constructor(props) {
@@ -9,38 +11,63 @@ class PrivateTransactions extends React.Component {
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
-    handleFormSubmit(e) {
-        e.preventDefault();
+    componentDidMount() {
+        console.log(curve25519);
+    }
 
+    handleFormSubmit(params) {
         const data = {
-            passphrase: this.refs.passphrase.value
+            passphrase: params.passphrase
         };
 
         this.props.setModalData(data);
-        console.log(data);
     }
 
     render() {
         return (
             <div className="modal-box">
-                <form className="modal-form" onSubmit={this.handleFormSubmit.bind(this)}>
-                    <div className="form-group">
-                        <div className="form-title">
-                            <p>Show private transactions</p>
-                        </div>
-                        <div className="input-group">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <label>Passphrase</label>
-                                </div>
-                                <div className="col-md-9">
-                                    <input ref={'passphrase'} type="text" name={'passphrase'}/>
+                <Form
+                    onSubmit={values => this.handleFormSubmit(values)}
+                    render={({
+                                   submitForm
+                               }) => (
+                    <form className="modal-form"  onSubmit={submitForm}>
+                        <div className="form-group">
+                            <div className="form-title">
+                                <p>Show private transactions</p>
+                            </div>
+                            <div className="input-group">
+                                <div className="row">
+                                    <div className="col-md-3">
+                                        <label>Passphrase</label>
+                                    </div>
+                                    <div className="col-md-9">
+                                        <Text field="passphrase" placeholder='Secret phrase' />
+                                    </div>
                                 </div>
                             </div>
+                            <button type="submit" className="btn btn-right">Enter</button>
                         </div>
-                        <button type="submit" className="btn btn-right">Enter</button>
-                    </div>
-                </form>
+                    </form>
+                )} />
+                {/*<form className="modal-form" onSubmit={this.handleFormSubmit.bind(this)}>*/}
+                    {/*<div className="form-group">*/}
+                        {/*<div className="form-title">*/}
+                            {/*<p>Show private transactions</p>*/}
+                        {/*</div>*/}
+                        {/*<div className="input-group">*/}
+                            {/*<div className="row">*/}
+                                {/*<div className="col-md-3">*/}
+                                    {/*<label>Passphrase</label>*/}
+                                {/*</div>*/}
+                                {/*<div className="col-md-9">*/}
+                                    {/*<input ref={'passphrase'} type="text" name={'passphrase'}/>*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
+                        {/*</div>*/}
+                        {/*<button type="submit" className="btn btn-right">Enter</button>*/}
+                    {/*</div>*/}
+                {/*</form>*/}
             </div>
         );
     }
