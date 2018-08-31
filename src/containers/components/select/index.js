a86891d73177e0297341d6515e588191725ab890
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-08-31 17:55:03 +0300

    Implemented create poll modal with form submitter

--- src/containers/components/select/index.js
+++ src/containers/components/select/index.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 
-const dot = (color = '#ccc') => ({
-    alignItems: 'center',
-    display: 'flex',
-
-    ':before': {
-        backgroundColor: color,
-        borderRadius: 10,
-        content: ' ',
-        display: 'block',
-        marginRight: 8,
-        height: 10,
-        width: 10,
-    },
-});
-
-const colourStyles = {
-    control: styles => ({ ...styles, backgroundColor: 'white'}),
-    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-        return {
-            ...styles,
-            backgroundColor: '#fff',
-            color: '#000',
-            cursor: isDisabled ? 'not-allowed' : 'default',
-            width: '100%'
-        };
-    },
-    input: styles => ({ ...styles, width: '100%',}),
-    placeholder: styles => ({ }),
-    singleValue: (styles, { data }) => ({ ...styles, width: '100%'}),
-};
+import './Select.css'
 
 class CustomSelect extends React.Component {
     constructor(props) {
@@ -40,8 +11,19 @@ class CustomSelect extends React.Component {
     render () {
         return (
             <Select
-                styles={colourStyles}
+                className={'custom-select'}
                 options={this.props.options}
+                onChange={(selectedOption) => {this.props.setValue(this.props.field, selectedOption.value)}}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colors: {
+                        ...theme.colors,
+                        text: 'orangered',
+                        primary25: 'hotpink',
+                        primary: 'black',
+                    },
+                })}
             />
         );
     }
