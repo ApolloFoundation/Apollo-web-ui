5f27cc85039bc05f19997a46afaa6414489856f5
Nikita Shevchenko <shevchenkona19@gmail.com>
2018-09-03 19:25:27 +0300

    added calculate hash modal

--- src/containers/modals/account/hash-calculation.js
+++ src/containers/modals/account/hash-calculation.js
@@ -4,8 +4,10 @@ import {setModalData} from '../../../modules/modals';
 import converters from '../../../helpers/converters';
 import classNames from 'classnames';
 
-import { Form, Text, TextArea, Checkbox } from 'react-form';
+import {Form, Text, TextArea, Checkbox, Select} from 'react-form';
 import InfoBox from '../../components/info-box';
+import submitForm from "../../../helpers/forms/forms";
+import {NotificationManager} from "react-notifications";
 
 class HashCalculation extends React.Component {
     constructor(props) {
@@ -21,37 +23,29 @@ class HashCalculation extends React.Component {
             passphraseStatus: false,
             recipientStatus: false,
             amountStatus: false,
-            feeStatus: false
-        }
-
-        this.handleTabChange = this.handleTabChange.bind(this);
+            feeStatus: false,
+            generatedHash: false,
+        };
         this.handleAdvancedState = this.handleAdvancedState.bind(this);
     }
 
     async handleFormSubmit(values) {
-        const isPassphrase = await this.props.validatePassphrase(values.secretPhrase);
-
-        if (!isPassphrase) {
-            this.setState({
-                ...this.props,
-                passphraseStatus: true
-            })
-            return;
-        } else {
-            this.setState({
-                ...this.props,
-                passphraseStatus: false
-            })
-        }
-
-        this.props.generateToken(values.message, values.secretPhrase);
-    }
-
-    handleTabChange(tab) {
-        this.setState({
-            ...this.props,
-            activeTab: tab
-        })
+        console.warn(values);
+        this.props.submitForm(null, null, {
+            secret: values.data,
+            secretIsText: values.isMessage,
+            hashAlgorithm: values.alg,
+            feeATM: 0
+        }, 'hash')
+            .done(res => {
+                if (res.errorCode) {
+                    NotificationManager.error(res.errorDescription, "Error", 5000)
+                } else {
+                    this.setState({
+                        generatedHash: res.hash
+                    });
+                }
+            });
     }
 
     handleAdvancedState() {
@@ -68,6 +62,33 @@ class HashCalculation extends React.Component {
         }
     }
 
+    hashOptions = [
+        {
+            label: "SHA256",
+            value: "2"
+        },
+        {
+            label: "SHA3",
+            value: "3",
+        },
+        {
+            label: "SCRYPT",
+            value: "5",
+        },
+        {
+            label: "RIPEMD160",
+            value: "6"
+        },
+        {
+            label: "Keccak25",
+            value: "25"
+        },
+        {
+            label: "RIPEMD160_SHA256",
+            value: "62"
+        }
+    ];
+
     render() {
         return (
             <div className="modal-box">
@@ -78,7 +99,8 @@ class HashCalculation extends React.Component {
                              }) => (
                         <form className="modal-form" onSubmit={submitForm}>
                             <div className="form-group">
-                                <a onClick={() => this.props.closeModal()} className="exit"><i className="zmdi zmdi-close" /></a>
+                                <a onClick={() => this.props.closeModal()} className="exit"><i
+                                    className="zmdi zmdi-close"/></a>
 
                                 <div className="form-title">
                                     <p>Hash calculation</p>
@@ -89,26 +111,24 @@ class HashCalculation extends React.Component {
                                             <label>Data</label>
                                         </div>
                                         <div className="col-md-9">
-                                            <TextArea rows={5} field="recipient" placeholder="Data to hash" />
+                                            <TextArea rows={5} field="data" placeholder="Data to hash"/>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="input-group offset-top display-block">
                                     <div className="row">
-                                        <div className="col-md-3" />
+                                        <div className="col-md-3"/>
                                         <div className="input-group align-middle display-block offset-bottom">
-                                            <Checkbox style={{display: 'inline-block'}} type="checkbox" field="isMessage"/>
-                                            <label style={{display: 'inline-block'}}>Remember passphrase for decryption</label>
+                                            <Checkbox style={{display: 'inline-block'}} type="checkbox"
+                                                      field="isMessage"/>
+                                            <label style={{display: 'inline-block'}}>Textual data representation</label>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="input-group offset-top display-block">
                                     <div className="row">
                                         <div className="col-md-3">
-                                            <label>SHA256</label>
-                                        </div>
-                                        <div className="col-md-9">
-                                            <Text field="feeATM" placeholder="Amount" />
+                                            <Select field="alg" options={this.hashOptions}/>
                                         </div>
                                     </div>
                                 </div>
@@ -137,25 +157,26 @@ class HashCalculation extends React.Component {
                                         Missing fee.
                                     </InfoBox>
                                 }
-
+                                {this.state.generatedHash ?
+                                    <InfoBox info>
+                                        {this.state.generatedHash}
+                                    </InfoBox> : null}
                                 <div className="btn-box align-buttons-inside absolute right-conner">
-                                    <button className="btn btn-right round round-top-left">Cancel</button>
                                     <button
-                                        type="submit"
-                                        name={'closeModal'}
-                                        className="btn btn-right blue round round-bottom-right"
+                                        onClick={() => this.props.closeModal()}
+                                        className="btn btn-right round round-top-left"
                                     >
-                                        Send
+                                        Cancel
                                     </button>
 
                                 </div>
                                 <div className="btn-box align-buttons-inside absolute left-conner">
-                                    <a
-                                        onClick={this.handleAdvancedState}
-                                        className="btn btn-right round round-bottom-left round-top-right"
+                                    <button
+                                        className="btn btn-right blue round round-bottom-right"
+                                        type="submit"
                                     >
                                         Calculate
-                                    </a>
+                                    </button>
                                 </div>
                             </div>
                         </form>
@@ -173,7 +194,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    setModalData: (data) => dispatch(setModalData(data))
+    setModalData: (data) => dispatch(setModalData(data)),
+    submitForm: (modal, btn, data, requestType) => dispatch(submitForm.submitForm(modal, btn, data, requestType)),
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HashCalculation);
