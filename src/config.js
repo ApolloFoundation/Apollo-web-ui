e9c6adb4c3b0402d95c34bff345901258c57bba9
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-09-11 14:24:09 +0300

    Changed config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'https://apolloface-reactdev.firstbridge.work:4443/apl?',
+    serverUrl: 'https://apolloface-reactdev.firstbridge.work/apl?',
     localServerUrl: 'http://localhost:3020'
 };
 
