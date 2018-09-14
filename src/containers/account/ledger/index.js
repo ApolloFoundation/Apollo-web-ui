7f55d525d97b6fd0cd3d23c12da36a43de472536
NickDoychev <nick.doychev@firstbridge.io>
2018-09-14 15:50:34 +0300

    fix

--- src/containers/account/ledger/index.js
+++ src/containers/account/ledger/index.js
@@ -219,30 +219,30 @@ class Ledger extends React.Component {
                                     }
                                     </tbody>
                                 </table>
-                                <div className="btn-box">
-                                    <a
-                                        className={classNames({
-                                            'btn' : true,
-                                            'btn-left' : true,
-                                            'disabled' : this.state.page <= 1
-                                        })}
-                                        onClick={this.onPaginate.bind(this, this.state.page - 1)}
-                                    > Previous</a>
-                                    <div className='pagination-nav'>
-                                        <span>{this.state.firstIndex + 1}</span>
-                                        <span>&hellip;</span>
-                                        <span>{this.state.lastIndex + 1}</span>
-                                    </div>
-                                    <a
-                                        onClick={this.onPaginate.bind(this, this.state.page + 1)}
-                                        className={classNames({
-                                            'btn' : true,
-                                            'btn-right' : true,
-                                            'disabled' : this.state.ledger.length < 15
-                                        })}
-                                    >Next</a>
-                                </div>
                             </div>
+	                        <div className="btn-box under-table">
+		                        <a
+			                        className={classNames({
+				                        'btn' : true,
+				                        'btn-left' : true,
+				                        'disabled' : this.state.page <= 1
+			                        })}
+			                        onClick={this.onPaginate.bind(this, this.state.page - 1)}
+		                        > Previous</a>
+		                        <div className='pagination-nav'>
+			                        <span>{this.state.firstIndex + 1}</span>
+			                        <span>&hellip;</span>
+			                        <span>{this.state.lastIndex + 1}</span>
+		                        </div>
+		                        <a
+			                        onClick={this.onPaginate.bind(this, this.state.page + 1)}
+			                        className={classNames({
+				                        'btn' : true,
+				                        'btn-right' : true,
+				                        'disabled' : this.state.ledger.length < 15
+			                        })}
+		                        >Next</a>
+	                        </div>
                         </div>
                     </div>
                 </div>
