05218bb26c81296114b3641d5105daad68effed1
Nikita Shevchenko <shevchenkona19@gmail.com>
2018-09-17 13:50:39 +0300

    fixes for exchange booth

--- src/containers/account/exchange-booth/exchange-item/ExchangeItem.js
+++ src/containers/account/exchange-booth/exchange-item/ExchangeItem.js
@@ -6,11 +6,11 @@ class ExchangeItem extends React.Component {
         const {exchange} = this.props;
         return (
             <tr>
-                <td className="blue-link-text"><a>{exchange.height}</a></td>
+                <td className="align-left">{exchange.height}</td>
                 <td className="align-right">{"buy"}</td>
-                <td className="align-right">{exchange.units / 100}</td>
-                <td className="align-right">{exchange.rateATM / 6}</td>
-                <td className="align-right">{exchange.rateATM / 6 * exchange.units / 100}</td>
+                <td className="align-right">{Math.ceil(exchange.units / 100)}</td>
+                <td className="align-right">{Math.ceil(exchange.rateATM / 6)}</td>
+                <td className="align-right">{Math.ceil(exchange.rateATM / 6 * exchange.units / 100)}</td>
             </tr>
         );
     }
