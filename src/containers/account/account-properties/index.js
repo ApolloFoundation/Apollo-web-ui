191b88bbaab6c7b2a020e51864e6b379f2ee1672
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-09-18 12:41:48 +0300

    Fixed account properties page

--- src/containers/account/account-properties/index.js
+++ src/containers/account/account-properties/index.js
@@ -1,7 +1,57 @@
 import React from 'react';
 import SiteHeader from '../../components/site-header'
+import {connect} from 'react-redux'
+import {getAccountPropertiesAction} from '../../../actions/account/index'
+import async from "../../../helpers/util/async";
+import InfoBox from '../../components/info-box'
+import {setBodyModalParamsAction} from "../../../modules/modals";
+
+const mapStateToProps = state => ({
+    account: state.account.account
+})
+
+const mapDisatchToProps = dispatch => ({
+    getAccountPropertiesAction: (requsetParams) => dispatch(getAccountPropertiesAction(requsetParams)),
+    setBodyModalParamsAction: (type, data) => dispatch(setBodyModalParamsAction(type, data)),
+})
 
 class AccountProperties extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        properties: null,
+        firstIndex: 0,
+        lastIndex: 14,
+        page: 1,
+
+    };
+
+    componentDidMount() {
+        this.getAccountPropertiesAction(this.props);
+    }
+
+    componentWillReceiveProps (newState) {
+        this.getAccountPropertiesAction(newState);
+    }
+
+    getAccountPropertiesAction = async (newState) => {
+        const properties = await this.props.getAccountPropertiesAction({
+            recipient: newState.account,
+            firstIndex: this.state.firstIndex,
+            lastIndex: this.state.lastIndex,
+        })
+
+        if (properties) {
+            console.log(properties);
+
+            this.setState({
+                properties: properties.properties
+            })
+        }
+    }
+
     render () {
         return (
             <div className="page-content">
@@ -14,52 +64,49 @@ class AccountProperties extends React.Component {
                             <div className="transaction-table-body">
                                 <table>
                                     <thead>
-                                    <tr>
-                                        <td>Setter</td>
-                                        <td>Property</td>
-                                        <td>Value</td>
-                                        <td className="align-right">Actions</td>
-                                    </tr>
+                                        <tr>
+                                            <td>Setter</td>
+                                            <td>Property</td>
+                                            <td>Value</td>
+                                            <td className="align-right">Actions</td>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    <tr>
-                                        <td className="blue-link-text"><a>You</a>
-                                        </td>
-                                        <td>example</td>
-                                        <td>test</td>
-                                        <td className="align-right">
-                                            <div className="btn-box inline"><a className="btn primary blue">Update</a><a
-                                                className="btn primary">Delete</a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="blue-link-text"><a>You</a>
-                                        </td>
-                                        <td>example</td>
-                                        <td>test</td>
-                                        <td className="align-right">
-                                            <div className="btn-box inline"><a className="btn primary blue">Update</a><a
-                                                className="btn primary">Delete</a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className="blue-link-text"><a>You</a>
-                                        </td>
-                                        <td>example</td>
-                                        <td>test</td>
-                                        <td className="align-right">
-                                            <div className="btn-box inline"><a className="btn primary blue">Update</a><a
-                                                className="btn primary">Delete</a>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                        {
+                                            this.state.properties &&
+                                            this.state.properties.length &&
+                                            this.state.properties.map((el) => {
+                                                return (
+                                                    <tr>
+                                                        <td className="blue-link-text">
+                                                            <a
+                                                                onClick={() => this.props.setBodyModalParamsAction('INFO_ACCOUNT', el.setter)}
+                                                            >
+                                                                {el.setterRS}
+                                                            </a>
+                                                        </td>
+                                                        <td>{el.property}</td>
+                                                        <td>{el.value}</td>
+                                                        <td className="align-right">
+                                                            <div className="btn-box inline"><a className="btn primary blue">Update</a><a
+                                                                className="btn primary">Delete</a>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                        {
+                                            this.state.properties &&
+                                            !!(!this.state.properties.length) &&
+                                            <InfoBox default>
+                                                No properties found.
+                                            </InfoBox>
+                                        }
+
                                     </tbody>
                                 </table>
-                                {/*// <!--.btn-box-->*/}
-                                {/*// <!--	a.btn.btn-left  Previous-->*/}
-                                {/*// <!--	a.btn.btn-right Next-->*/}
+
                             </div>
                         </div>
                     </div>
@@ -69,4 +116,4 @@ class AccountProperties extends React.Component {
     }
 }
 
-export default AccountProperties;
+export default connect(mapStateToProps, mapDisatchToProps)(AccountProperties);
