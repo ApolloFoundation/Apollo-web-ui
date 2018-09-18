138958195d73225ce48ea9f219f8dfd91f03d7b2
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2018-09-18 11:47:07 +0300

    Redesign Issue currency modal

--- src/containers/modals/messenger/compose-message.js
+++ src/containers/modals/messenger/compose-message.js
@@ -253,7 +253,7 @@ class ComposeMessage extends React.Component {
                                         className="btn btn-right round round-bottom-left round-top-right absolute"
                                         style={{left : 0, right: 'auto'}}
                                     >
-                                        Advanced
+                                        {this.state.advancedState ? "Basic" : "Advanced"}
                                     </a>
                                 </div>
                                 <AdvancedSettings
