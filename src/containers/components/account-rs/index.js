77e2159ad309f91b0d437102f7a336b2098b77b8
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2018-09-19 18:52:38 +0300

    Fix Account field on Alias modal

--- src/containers/components/account-rs/index.js
+++ src/containers/components/account-rs/index.js
@@ -74,7 +74,7 @@ class AccountRS extends React.Component {
                     <InputMask className="form-control"
                                disabled={this.props.disabled}
                                mask={this.state.inputValue.mask}
-                               placeholder={'Recipient account'}
+                               placeholder={this.props.placeholder || 'Recipient account'}
                                ref={'input'}
                                value={this.state.inputValue.value}
                                onPaste={this.onChange}
