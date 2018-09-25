0ca43b897c283768e6777bbb128ad6057debfe62
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-09-25 10:52:01 +0300

    Fixed user autosaving

--- src/actions/login/index.js
+++ src/actions/login/index.js
@@ -55,7 +55,7 @@ export function getAccountDataBySecretPhrasseAction(requestParams) {
                 localContacts = JSON.parse(localContacts);
 
                 if (localContacts) {
-                    const isInside = localContacts.find((el) => {
+                    const isInside = localContac.ts.find((el) => {
                         return el.accountRS === values.accountRS
                     });
 
