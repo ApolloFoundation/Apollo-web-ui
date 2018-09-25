c9d89335516b0fe43813c037790d903c19b7f4d9
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2018-09-25 10:54:42 +0300

    Fix IE modals

--- src/helpers/closest.js
+++ src/helpers/closest.js
@@ -1,3 +1,7 @@
+/**
+ * Element.closest() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
+ */
 if (!Element.prototype.closest) {
     if (!Element.prototype.matches) {
         Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
