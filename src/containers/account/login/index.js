42c541b07002cd79691c8989ffb963e6e2ae34ad
greg <hryhorii.chaikovskyi@firstbridge.io>
2018-09-28 19:28:53 +0300

    Fixed account rs AGAIN. Added copyrights at the login page

--- src/containers/account/login/index.js
+++ src/containers/account/login/index.js
@@ -15,6 +15,7 @@ import {setBodyModalParamsAction} from "../../../modules/modals";
 import classNames from "classnames";
 import AccountRS from '../../components/account-rs';
 import {Form, Text} from "react-form";
+import {getConstantsAction} from '../../../actions/login'
 
 class Login extends React.Component {
     constructor(props) {
@@ -24,6 +25,10 @@ class Login extends React.Component {
     state = {
         activeTab: 0
     };
+
+    componentDidMount () {
+        this.props.getConstantsAction();
+    }
 
     enterAccount = (values) => {
         this.props.getAccountAction({
@@ -96,6 +101,7 @@ class Login extends React.Component {
                                                                     value={''}
                                                                     field={'accountRS'}
                                                                     setValue={setValue}
+                                                                    placeholder={'Account ID'}
                                                                 />
                                                             </div>
                                                         </div>
@@ -160,8 +166,21 @@ class Login extends React.Component {
                                         )}
                                     />
                                 </div>
+
                             </div>
                         </div>
+                        <p
+                            style={{
+                                position: "absolute",
+                                bottom: "30px",
+                                color: 'white',
+                                width: '100%',
+                                textAlign: 'center',
+                                left: '0'
+                            }}
+                        >
+                            Copyright © 2017-2018 Apollo Foundation. Apollo Version: {!!this.props.appState && this.props.appState.version} <br/>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -170,14 +189,16 @@ class Login extends React.Component {
 }
 
 const mapStateToProps = state =>  ({
-    account: state.account
+    account: state.account,
+    appState: state.account.blockchainStatus
 });
 
 const mapDipatchToProps = dispatch => {
     return {
         getAccountAction: (requestParams) => dispatch(getAccountDataAction(requestParams)),
         getAccountDataBySecretPhrasseAction: (requestParams) => dispatch(getAccountDataBySecretPhrasseAction(requestParams)),
-        setBodyModalParamsAction: (type, data) => dispatch(setBodyModalParamsAction(type, data))
+        setBodyModalParamsAction: (type, data) => dispatch(setBodyModalParamsAction(type, data)),
+        getConstantsAction: () => dispatch(getConstantsAction()),
     };
 };
 
