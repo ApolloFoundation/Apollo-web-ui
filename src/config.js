f7eea077d5e5da97a10969e3be15dbcc65676aa4
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2018-10-26 12:55:23 +0300

    Fixed production url
    Signed-off-by: Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'http://localhost:6876/apl?',
+    serverUrl: '/apl?',
     localServerUrl: 'https://apollowallet.org/api/twitter'
 };
 
