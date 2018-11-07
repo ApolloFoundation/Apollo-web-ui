a3954c07c1225c937b9f8bf110d13e369172b493
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2018-11-07 15:10:19 +0200

    Added functionality of shuffling with Vault wallets
    Signed-off-by: Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>

--- src/containers/modals/coin-shuffling/join-shuffling.js
+++ src/containers/modals/coin-shuffling/join-shuffling.js
@@ -9,7 +9,7 @@ import {connect} from 'react-redux';
 import {setBodyModalParamsAction, setModalData, saveSendModalState, openPrevModal} from '../../../modules/modals';
 import AdvancedSettings from '../../components/advanced-transaction-settings';
 import InputForm from '../../components/input-form';
-import {Form, Text} from 'react-form';
+import {Checkbox, Form, Text} from 'react-form';
 import {getBlockAction} from "../../../actions/blocks";
 import {NotificationManager} from "react-notifications";
 import submitForm from "../../../helpers/forms/forms";
@@ -20,6 +20,9 @@ import crypto from "../../../helpers/crypto/crypto";
 import ModalFooter from '../../components/modal-footer'
 
 import BackForm from '../modal-form/modal-form-container';
+import InfoBox from "../../components/info-box";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import {generateAccountAction} from "../../../actions/account";
 
 class JoinShuffling extends React.Component {
     constructor(props) {
@@ -39,7 +42,7 @@ class JoinShuffling extends React.Component {
     }
 
     handleFormSubmit = async(values) => {
-        const data = {
+        let data = {
             shufflingFullHash: this.props.modalData.broadcast ? this.props.modalData.broadcast.fullHash : this.state.shuffling.shufflingFullHash,
             recipientSecretPhrase: values.recipientSecretPhrase,
             secretPhrase: values.secretPhrase,
@@ -47,6 +50,13 @@ class JoinShuffling extends React.Component {
             createNoneTransactionMethod: true,
             code2FA: values.code2FA
         };
+
+        if (values.isVaultWallet) {
+            data.recipientAccount = this.state.vaultWallet.accountRS;
+            data.recipientPassphrase = this.state.vaultWallet.passphrase;
+
+            delete data.recipientSecretPhrase;
+        }
 
         this.setState({
             isPending: true
@@ -120,6 +130,19 @@ class JoinShuffling extends React.Component {
         setValue('generatedAccount', generatedAccount);
     };
 
+    handleVaultWalletCondition = async (condition) => {
+        console.log(condition);
+        if (condition) {
+            const vaultWallet = await generateAccountAction({});
+
+            if (vaultWallet) {
+                this.setState({
+                    vaultWallet
+                })
+            }
+        }
+    };
+
     render() {
         return (
             <div className="modal-box">
@@ -152,36 +175,96 @@ class JoinShuffling extends React.Component {
                                         }
                                     </div>
                                 </div>
-                                <div className="form-group row form-group-white mb-15">
-                                    <label className="col-sm-3 col-form-label">
-                                        Recipient secret phrase
-                                    </label>
-                                    <div className="col-sm-9">
-                                        <Text className="form-control"
-                                              field="recipientSecretPhrase"
-                                              placeholder="Account ID"
-                                              onKeyUp={(e) => {
-                                                  if (getFormState().values.recipientSecretPhrase !== '') {
-                                                      this.setAccount(getFormState, setValue)
-                                                  } else {
-                                                      setValue('generatedAccount', '')
-                                                  }
-                                              }}
-                                              type={'password'}/>
+                                {
+                                    !getFormState().values.isVaultWallet &&
+                                    <div className="form-group row form-group-white mb-15">
+                                        <label className="col-sm-3 col-form-label">
+                                            Recipient secret phrase
+                                        </label>
+                                        <div className="col-sm-9">
+                                            <Text className="form-control"
+                                                  field="recipientSecretPhrase"
+                                                  placeholder="Account ID"
+                                                  onKeyUp={(e) => {
+                                                      if (getFormState().values.recipientSecretPhrase !== '') {
+                                                          this.setAccount(getFormState, setValue)
+                                                      } else {
+                                                          setValue('generatedAccount', '')
+                                                      }
+                                                  }}
+                                                  type={'password'}/>
+                                        </div>
+                                    </div>
+                                }
+                                <div className="mobile-class row mb-15 form-group-white">
+                                    <div className="col-md-9 offset-md-3 pl-0">
+                                        <div className="form-check custom-checkbox single">
+                                            <Checkbox
+                                                style={{display: 'inline-block'}}
+                                                defaultValue={false}
+                                                type="checkbox"
+                                                field="isVaultWallet"
+                                                onChange={(e) => this.handleVaultWalletCondition(e)}
+                                            />
+                                            <label className="form-check-label custom-control-label">
+                                                Use Vault Wallet for Shuffling
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="form-group row form-group-white mb-15">
-                                    <label className="col-sm-3 col-form-label">
-                                        Recipient Account
-                                    </label>
-                                    <div className="col-sm-9 mb-0">
-                                        <InputForm
-                                            disabled={true}
-                                            field="generatedAccount"
-                                            placeholder="Account ID"
-                                            setValue={setValue}/>
+
+                                <div className="mobile-class row mb-15 form-group-white">
+                                    <div className="col-md-9 offset-md-3 pl-0">
+                                        {
+                                            getFormState().values.isVaultWallet &&
+                                            this.state.vaultWallet &&
+                                            <InfoBox attentionLeft>
+                                                Secret Phrase:  <span className={'itatic'}>{this.state.vaultWallet.passphrase}</span>
+                                                <br/>
+                                                <br/>
+                                                Account ID: <span className={'itatic'}>{this.state.vaultWallet.accountRS}</span>
+                                                <br/>
+                                                <br/>
+                                                Public Key: <span className={'itatic'}>{this.state.vaultWallet.publicKey}</span>
+                                                <br/>
+                                                <br/>
+                                                <CopyToClipboard
+                                                    text={
+                                                        `Secret Phrase: ${this.state.vaultWallet.passphrase}\n` +
+                                                        `Account ID: ${this.state.vaultWallet.accountRS}\n` +
+                                                        `Public Key: ${this.state.vaultWallet.publicKey}\n`
+                                                    }
+                                                    onCopy={() => {
+                                                        NotificationManager.success('The account data has been copied to clipboard.')
+                                                    }}
+                                                >
+                                                    <a
+                                                        className="btn blue static"
+                                                    >
+                                                        Copy account data to clipboard.
+                                                    </a>
+                                                </CopyToClipboard>
+                                            </InfoBox>
+                                        }
                                     </div>
                                 </div>
+
+                                {
+                                    !getFormState().values.isVaultWallet &&
+                                    <div className="form-group row form-group-white mb-15">
+                                        <label className="col-sm-3 col-form-label">
+                                            Recipient Account
+                                        </label>
+                                        <div className="col-sm-9 mb-0">
+                                            <InputForm
+                                                disabled={true}
+                                                field="generatedAccount"
+                                                placeholder="Account ID"
+                                                setValue={setValue}/>
+                                        </div>
+                                    </div>
+                                }
+
                                 <ModalFooter
                                     setValue={setValue}
                                     getFormState={getFormState}
