cf89177695d030803b3b79c8e1f7d837ba9214e3
nikitashevchenko <shevchenkona19@gmail.com>
2018-11-08 18:18:03 +0200

    added new modals

---
+++ src/containers/modals/info-transaction/table-content/alias-sell.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+
+export default class AliasSell extends Component {
+    render() {
+        const atch = this.props.transaction.attachment;
+        return (
+            <React.Fragment>
+                <tr>
+                    <td>Alias Name:</td>
+                    <td>{atch.alias}</td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td>{atch.priceATM}</td>
+                </tr>
+                <tr>
+                    <td>Recipient:</td>
+                    <td>{this.props.transaction.recipientRS}</td>
+                </tr>
+                <tr>
+                    <td>Sender:</td>
+                    <td>{this.props.transaction.senderRS}</td>
+                </tr>
+            </React.Fragment>
+        );
+    }
+}
