06c1e630e347dea482d5a5e2930468969224c29f
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2018-11-22 19:37:10 +0200

    Changed server url

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'http://51.15.235.41:6876/apl?',
+    serverUrl: '/apl?',
     localServerUrl: 'https://apollowallet.org/api/twitter'
 };
 
