51c3bc1198d30ffff102c76c31d1b44435749b69
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2018-12-12 21:14:36 +0200

    Fixed config

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'http://localhost:6876/apl?',
+    serverUrl: '/apl?',
     localServerUrl: 'https://apollocurrency.com/api/client/twitter'
 };
 
