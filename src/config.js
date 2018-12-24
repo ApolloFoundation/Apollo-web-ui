27f0a2c73f0c3220e635acb94d95b3430470e6e9
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2018-12-24 21:59:27 +0200

    Fixed getting mixer

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'http://51.15.242.197:6876/apl?',
+    serverUrl: '/apl?',
     localServerUrl: 'https://apollowallet.org/api/twitter',
     mixerUrl: 'http://51.15.72.23:8080/api/get-account'
 };
