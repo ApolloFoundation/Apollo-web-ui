d2b54d232d64277b0b0eb360efcfd11275e27e7d
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-02-14 17:04:17 +0200

    Added dashboard duck

---
+++ src/modules/dashboard.js
@@ -0,0 +1,72 @@
+const initialState = {
+    dashboardTransactions : null
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_DASHBOARD_TRANSACTIONS':
+            return {
+                ...state,
+                dashboardTransactions: action.payload
+            };
+        case 'SET_DASHBOARD_ASSETS':
+            return {
+                ...state,
+                dashboardAssets: action.payload,
+            }
+        case 'SET_DASHBOARD_CURRENCIES':
+            return {
+                ...state,
+                dashboardCurrencies: action.payload,
+            }
+        case 'SET_DASHBOARD_ALIASES_COUNT':
+            return {
+                ...state,
+                dashboardAliasesCount: action.payload,
+            }
+        case 'SET_DASHBOARD_DGS_GOODS':
+            return {
+                ...state,
+                dashboardDgsGoods: action.payload,
+            }    
+        case 'SET_DASHBOARD_MESSAGES_COUNT':
+            return {
+                ...state,
+                dashboardMessagesCount: action.payload,
+            }
+        case 'SET_DASHBOARD_NEWS':
+            return {
+                ...state,
+                dashboardNews: action.payload,
+            }
+        case 'SET_DASHBOARD_TAGGEDDATA':
+            return {
+                ...state,
+                dashboardTaggedData: action.payload,
+            }
+        case 'SET_DASHBOARD_ACTIVE_SHUFFLING': 
+            return {
+                ...state,
+                dashboardActiveSuffling: action.payload,
+            }
+        case 'SET_DASHBOARD_POSSL': 
+            return {
+                ...state,
+                dashboardActivePolls: action.payload,
+            }
+        case 'SET_DASHBOARD_ACCOUNT_INFO': 
+            return {
+                ...state,
+                dashboardAccoountInfo: action.payload,
+            }
+        default:
+            return state
+    }
+}
+
+export const setDashboardTransactions = dispatch => (transactions) => {
+    dispatch({
+        type: 'SET_DASHBOARD_TRANSACTIONS',
+        payload: transactions
+    })
+}
--- src/modules/index.js
+++ src/modules/index.js
@@ -14,6 +14,7 @@ import settings from "./settings"
 import accountSettings from "./accountSettings"
 import currencies from "./currencies"
 import ledger from "./ledger"
+import dashboard from "./dashboard"
 
 export default combineReducers({
 	routing: routerReducer,
@@ -23,6 +24,7 @@ export default combineReducers({
     fee,
     settings,
     currencies,
+    dashboard,
     ledger,
     accountSettings,
 })
