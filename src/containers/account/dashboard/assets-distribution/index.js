5ceef14319d38c7aa5962812724e533fb0283315
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-02-26 15:50:20 +0200

    Fixed asset portfolio

--- src/containers/account/dashboard/assets-distribution/index.js
+++ src/containers/account/dashboard/assets-distribution/index.js
@@ -8,40 +8,6 @@ import CircleFigure from '../circle-figure';
 class AssetsDistribution extends Component {
     state = {};
 
-    componentDidMount = () => {
-        this.getAssets();
-    }
-
-    getAssets = async (newState) => {
-		let assets = await this.props.getAssetAction({
-			account: this.props.account
-		});
-		if (assets) {
-			const accountAssets = assets.accountAssets;
-			const assetsInfo    = assets.assets;
-
-
-			const result = accountAssets.map((el, index) => {
-				return {...(assetsInfo[index]), ...el}
-			});
-
-            let assetsCountValue;
-
-            if (!!result.length) {
-                assetsCountValue = (result.map((el) => {return Number(el.quantityATU / Math.pow(10, el.decimals))})).reduce((a, b) => {return a + b});
-			} else {
-                assetsCountValue = 0;
-            }
-            
-            console.log(result)
-
-            this.setState({
-				dashboardAssets: result.splice(0,3),
-                assetsValue: assetsCountValue
-			})
-		}
-    };
-
     render () {
         const {dashboardAssets} = this.props;
 
@@ -83,9 +49,10 @@ class AssetsDistribution extends Component {
                             }
                         })
                     }
+                    {console.log(dashboardAssets)}
                     {
                         !!dashboardAssets &&
-                        dashboardAssets.length === 0 &&
+                        dashboardAssets.count === 0 &&
                         <p>No assets found.</p>
                     }
                 </div>
