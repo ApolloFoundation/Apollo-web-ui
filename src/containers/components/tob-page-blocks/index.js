f738c159d977b5c0a77d43fb32f232c14f68437a
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-03-04 14:17:35 +0200

    Fixed fixed dblocks

--- src/containers/components/tob-page-blocks/index.js
+++ src/containers/components/tob-page-blocks/index.js
@@ -4,7 +4,7 @@ const TopPageBlocks = ({cards}) => (
     <div className="row top-blocks ">
         {
             cards.map((el, index) => 
-                <div className="col-md-6 col-lg-6 col-xl-3 pb-3 h-100">
+                <div className="col-md-6 col-lg-6 col-xl-3 pb-3">
                     <div className={`card header ballance justify-content-start h-100 chart-sprite position-${index + 1} ${index === 0 ? 'ballance' : ''} ${index === 1 ? 'assets' : ''} ${index === 2 ? 'currencies' : ''} ${index === 3 ? 'coins' : ''}`}>
                         {
                             !(el.value instanceof Array) && 
