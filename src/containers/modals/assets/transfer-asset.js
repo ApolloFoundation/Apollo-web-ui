d4eb855cf0753517c0e79de9d5de2332a6cb5f65
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-03-05 16:23:24 +0200

    Fixed transfer asset

--- src/containers/modals/assets/transfer-asset.js
+++ src/containers/modals/assets/transfer-asset.js
@@ -16,12 +16,18 @@ import {NotificationManager} from "react-notifications";
 import ModalFooter from '../../components/modal-footer'
 import FeeCalc from '../../components/form-components/fee-calc';
 
+import ModalBody from '../../components/modals/modal-body';
+import AssetInput from '../../components/form-components/asset-input';
+import AccountRsInput from '../../components/form-components/account-rs';
+import TextualInputComponent from '../../components/form-components/textual-input';
+import NumericInputComponent from '../../components/form-components/numeric-input';
+import CustomTextArea from '../../components/form-components/text-area';
+
 class TransferAsset extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            activeTab: 0,
             advancedState: false,
 
             // submitting
@@ -34,8 +40,10 @@ class TransferAsset extends React.Component {
     }
 
     handleFormSubmit = async(values) => {
+        console.log(values)
         values = {
             ...values,
+            asset: values.assetID,
             quantityATU: values.quantityATU * Math.pow(10, this.props.modalData.decimals)
         }
 
@@ -45,134 +53,44 @@ class TransferAsset extends React.Component {
         });
     };
 
-    handleAdvancedState = () => {
-        if (this.state.advancedState) {
-            this.setState({
-                ...this.props,
-                advancedState: false
-            })
-        } else {
-            this.setState({
-                ...this.props,
-                advancedState: true
-            })
-        }
-    };
-
     render() {
         return (
-            <div className="modal-box">
-                <Form
-                    onSubmit={(values) => this.handleFormSubmit(values)}
-                    render={({ submitForm, values, addValue, removeValue, setValue, getFormState }) => (
-                        <form className="modal-form" onSubmit={submitForm}>
-                            {
-                                this.props.modalData &&
+            <ModalBody
+                loadForm={this.loadForm}
+                modalTitle={'Transfer Asset'}
+                isAdvanced={true}
+                isFee
+                closeModal={this.props.closeModal}
+                handleFormSubmit={(values) => this.handleFormSubmit(values)}
+                submitButtonName={'Transfer Asset'}
+                nameModel={this.props.nameModal}
+            >
+                <Text defaultValue={this.props.modalData.assetName} type="hidden" field={'name'}/>
+                <Text defaultValue={this.props.modalData.assetID} type="hidden" field={'asset'}/>
 
-                                <div className="form-group-app">
-                                    <a onClick={() => this.props.closeModal()} className="exit"><i className="zmdi zmdi-close" /></a>
-
-                                    <div className="form-title">
-                                        <p>Transfer Asset</p>
-                                    </div>
-                                    <div className="input-group-app display-block offset-bottom">
-                                        <div className="row">
-                                            <div className="col-md-3">
-                                                <label>Asset</label>
-                                            </div>
-                                            <div className="col-md-9">
-                                                <Text defaultValue={this.props.modalData.assetName} type="hidden" field={'name'}/>
-                                                <Text defaultValue={this.props.modalData.assetID} type="hidden" field={'asset'}/>
-                                                <p>{this.props.modalData.assetName} - {this.props.modalData.availableAssets} available</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="input-group-app display-block offset-bottom inline user">
-                                        <div className="row">
-                                            <div className="col-md-3">
-                                                <label>Recipient</label>
-                                            </div>
-                                            <div className="col-md-9">
-                                                <div className="iconned-input-field">
-                                                    <AccountRS
-                                                        field={'recipient'}
-                                                        setValue={setValue}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="input-group-app display-block offset-bottom">
-                                        <div className="row">
-                                            <div className="col-md-3">
-                                                <label>Quantity</label>
-                                            </div>
-                                            <div className="col-md-9">
-                                                <Text placeholder={'Quantity'} type="text" field={'quantityATU'}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <FeeCalc
-                                        values={getFormState().values}
-                                        setValue={setValue}
-                                        requestType={'transferAsset'}
-                                    />
-                                    <ModalFooter
-                                        setValue={setValue}
-                                        getFormState={getFormState}
-                                        values={values}
-                                    />
-
-                                    <AdvancedSettings
-                                        setValue={setValue}
-                                        getFormState={getFormState}
-                                        values={values}
-                                        advancedState={this.state.advancedState}
-                                    />
-
-                                    <div className="btn-box align-buttons-inside absolute right-conner">
-
-                                        {
-                                            !!this.state.isPending ?
-                                                <div
-                                                    style={{
-                                                        width: 120
-                                                    }}
-                                                    className="btn btn-right blue round round-bottom-right"
-                                                >
-                                                    <div className="ball-pulse">
-                                                        <div></div>
-                                                        <div></div>
-                                                        <div></div>
-                                                    </div>
-                                                </div> :
-                                                <button
-                                                    style={{
-                                                        width: 120
-                                                    }}
-                                                    type="submit"
-                                                    name={'closeModal'}
-                                                    className="btn btn-right blue round round-bottom-right"
-                                                >
-                                                    Transfer Asset
-                                                </button>
-                                        }
-                                        <a onClick={() => this.props.closeModal()} className="btn btn-right round round-top-left">Cancel</a>
-                                    </div>
-                                   {/*<div className="btn-box align-buttons-inside absolute left-conner">*/}
-                                        {/*<a*/}
-                                            {/*onClick={this.handleAdvancedState}*/}
-                                            {/*className="btn btn-left round round-bottom-left round-top-right"*/}
-                                        {/*>*/}
-                                            {/*{this.state.advancedState ? "Basic" : "Advanced"}*/}
-                                        {/*</a>*/}
-                                    {/*</div>*/}
-                                </div>
-                            }
-                        </form>
-                    )}
+                <TextualInputComponent
+                    isText
+                    label={'Asset'}
+                    text={`${this.props.modalData.assetName} - ${this.props.modalData.availableAssets} available`}
                 />
-            </div>
+
+                <AccountRsInput
+                    label={'Recipient'}
+                    field={'recipient'}
+                />      
+
+                <CustomTextArea
+                    label={'Comment'}
+                    field={'message'}
+                    placeholder={'Comment'}
+                />
+
+                <NumericInputComponent
+                    label={'Quantity'}
+                    placeholder={'Quantity'}
+                    field={'quantityATU'}
+                />
+            </ModalBody>
         );
     }
 }
