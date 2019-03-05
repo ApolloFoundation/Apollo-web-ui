1307c3ca24f7e244134931fa6609ca31f32bc00b
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-03-05 11:33:45 +0200

    Fixed config

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    serverUrl: 'http://51.15.235.41:7876/apl?',
+    serverUrl: '/apl?',
     localServerUrl: 'https://apollocurrency.com/api/client/twitter',
     mixerUrl: 'https://apollowallet.org/mixer/api/get-account',
     transportUrl: 'ws://127.0.0.1:8888/'
