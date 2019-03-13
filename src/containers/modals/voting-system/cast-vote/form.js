8e8f4986d4300b7e0805e5e184755efb409acb2e
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-03-13 13:09:58 +0200

    Fixed range button

--- src/containers/modals/voting-system/cast-vote/form.js
+++ src/containers/modals/voting-system/cast-vote/form.js
@@ -5,7 +5,7 @@ import {CheckboxFormInput} from '../../../components/form-components/check-butto
 import CustomRange from '../range';
 import ContentLoader from '../../../components/content-loader';
 
-const CastVoteForm = ({currencyHint, assetHint, poll, votes}) => (
+const CastVoteForm = ({currencyHint, assetHint, poll, votes, getFormState, setValue}) => (
     <>
         {
             poll && 
@@ -33,17 +33,25 @@ const CastVoteForm = ({currencyHint, assetHint, poll, votes}) => (
                     poll && 
                     votes &&
                     poll.maxRangeValue > 1 &&
-                    <div>
-                        {Object.keys(votes).map((el) =>
-                            <CustomRange 
-                                // setValue={setValue}
-                                label={votes[el]}
-                                min={poll.minRangeValue}
-                                max={poll.maxRangeValue}
-                                el={el}
-                                // getFormState={getFormState}
-                            />
-                        )}
+                    <div className="row form-group-grey">
+                        <div className={`col-md-3 pl-0`}>
+                            <span>Select option</span>
+                        </div>
+                        
+                        <div className={`col-md-9 offset-md-3`}>
+                            <div className="form-check custom-checkbox mb-15">
+                                {Object.keys(votes).map((el) =>
+                                    <CustomRange 
+                                        setValue={setValue}
+                                        label={votes[el]}
+                                        min={poll.minRangeValue}
+                                        max={poll.maxRangeValue}
+                                        el={el}
+                                        getFormState={getFormState}
+                                    />
+                                )}
+                            </div>
+                        </div>
                     </div>
                     
                 }
@@ -51,6 +59,7 @@ const CastVoteForm = ({currencyHint, assetHint, poll, votes}) => (
                     poll && 
                     votes &&
                     poll.maxRangeValue === 1 &&
+                    
                     <CheckboxFormInput 
                         label={'Select option'}
                         checkboxes={Object.keys(votes).map((el, index) => {
