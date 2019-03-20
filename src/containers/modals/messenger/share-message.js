b287665b40615b2b3c198b4cdcc85a7b6ce99d0a
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@firstbridge.io>
2019-03-20 15:46:50 +0200

    Fixed share message name

--- src/containers/modals/messenger/share-message.js
+++ src/containers/modals/messenger/share-message.js
@@ -5,47 +5,79 @@ import InputForm from "../../components/input-form";
 import ModalFooter from "../../components/modal-footer";
 import {setBodyModalParamsAction} from "../../../modules/modals";
 
+import FormFooter from '../../components/form-components/form-footer';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {NotificationManager} from "react-notifications";
+
 class ShareMessage extends React.Component {
     render() {
         const data = this.props.modalData;
+        const lnik = `${window.location.protocol}//${window.location.host}?isShareMessage=true&account=${this.props.account}&transaction=${data.transaction}`
+
         return (
-            <div className="modal-box">
-                <Form
-                    onSubmit={(values) => this.handleFormSubmit(values)}
-                    render={({submitForm, values, addValue, removeValue, setValue, getFormState}) => (
-                        <form className="modal-form" onSubmit={submitForm}>
+            <div className="modal-box x-wide">
+                        <div className="modal-form">
                             <div className="form-group-app">
                                 <a onClick={() => this.props.closeModal()} className="exit"><i
                                     className="zmdi zmdi-close"/></a>
                                 <div className="form-title">
                                     <p>Share message</p>
                                 </div>
-                                <div className="form-group row form-group-white mb-15">
-                                    <label className="col-sm-3 col-form-label align-self-start">
-                                        Transaction
-                                    </label>
-                                    <div className="col-sm-9">
-                                        <a onClick={() => this.props.setBodyModalParamsAction('INFO_TRANSACTION', data.transaction)}>
-                                            {data.transaction}
-                                        </a>
+                                <div className="transaction-table no-min-height transparent">
+                                        <div className="transaction-table-body transparent full-info">
+                                            <table>
+                                                <tbody>
+                                                <tr>
+                                                    <td>Transaction:</td>
+                                                    <td>
+                                                        <a 
+                                                            onClick={() => setBodyModalParamsAction('INFO_TRANSACTION', data.transaction)}
+                                                            className={'blue-link-text'}
+                                                        >
+                                                            {data.transaction}
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Share Link:</td>
+                                                    <td className={'no-white-space'}>
+                                                        <div className={'d-md-flex d-sm-dlock'}>
+
+                                                            <a 
+                                                                className={'blue-text'}
+                                                                href={lnik}
+                                                                rel="noopener noreferrer"
+                                                                target="_blank">
+                                                                {lnik}
+                                                            </a>
+
+                                                            <CopyToClipboard
+                                                                text={lnik}
+                                                                onCopy={() => {
+                                                                    NotificationManager.success('The share link has been copied to clipboard.')
+                                                                }}
+                                                            >
+                                                                <a
+                                                                    className="btn blue user-account-rs ml-md-3 mt-md-0 mt-sm-3 b-inline-block"
+                                                                >
+                                                                    Copy to clipboard
+                                                                </a>
+                                                            </CopyToClipboard>
+                                                        </div>
+                                                        
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="form-group row form-group-white mb-15">
-                                    <label className="col-sm-3 col-form-label align-self-start">
-                                        Share Link
-                                    </label>
-                                    <div className="col-sm-9">
-                                        <a href={`${window.location.protocol}//${window.location.host}?isShareMessage=true&account=${this.props.account}&transaction=${data.transaction}`}
-                                           rel="noopener noreferrer"
-                                           target="_blank">
-                                            {`${window.location.protocol}//${window.location.host}?isShareMessage=true&account=${this.props.account}&transaction=${data.transaction}`}
-                                        </a>
-                                    </div>
-                                </div>
+                                    <FormFooter 
+                                        closeModal={() => this.props.closeModal()}
+                                    />
                             </div>
-                        </form>
-                    )}
-                />
+                            
+                        </div>
+                    
             </div>
         )
     }
