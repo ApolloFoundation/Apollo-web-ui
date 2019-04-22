7a8caff31f3bdfb39de0333d465d86fb4d30c2eb
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2019-04-22 10:58:30 +0300

    Change copyright

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -351,7 +351,7 @@ class App extends React.Component {
                     {
                         !this.props.loading && !isLoginPage &&
                         <div className="site-footer">
-                            Copyright © 2017-2018 Apollo Foundation.
+                            Copyright © 2017-2019 Apollo Foundation.
                             <br className={'show-media hide-desktop'}/>
                             Apollo Version: {!!this.props.appState && this.props.appState.version} <br/>
                         </div>
