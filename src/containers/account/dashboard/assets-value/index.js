38ee4dd8fdc0978f2841b6cf86b3ee2d5a869670
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2019-05-10 15:00:30 +0300

    AWW-1015 Update style for top dashboard cards

--- src/containers/account/dashboard/assets-value/index.js
+++ src/containers/account/dashboard/assets-value/index.js
@@ -5,52 +5,52 @@ import {Link} from 'react-router-dom';
 
 class AssetsValue extends Component {
 
-    render () {
+    render() {
         const {dashboardAssets, positionState2, position2} = this.props;
 
         return (
             <div
-                className={`card header assets chart-sprite justify-content-start position-2 ${positionState2 ? "show-hide-content" : ""}`}>
+                className={`card ${positionState2 ? "show-hide-content" : ""}`}>
                 <div className="arrow-block" onClick={position2}>
                     <div className="arrow"/>
                 </div>
-                <div className="card-title">Assets Value</div>
-                {
-                    dashboardAssets &&
-                    <div className="page-body-item-content">
+                <div className="card-title card-title-lg bg-primary">Assets Value</div>
+                <div className="card-body">
+                    {dashboardAssets && (
                         <Link
                             to={'/my-assets'}
-                            style={{display: 'block'}}
-                            className="amount"
+                            className="amount d-flex justify-content-between align-items-center h-100"
                         >
-                            <div className="text">
+                            <div className="text-lg text-green">
                                 {Math.round(dashboardAssets.total).toLocaleString('en')}
                             </div>
-                            {
-                                Math.round(dashboardAssets.total) > 100000000000 &&
-                                <div className="amount-tooltip">
-                                    <div className="amount-tooltip-text">
-                                        {Math.round(this.state.assetsValue).toLocaleString('en')}
+                            <div className="text-lg text-green">
+                                {
+                                    Math.round(dashboardAssets.total) > 100000000000 &&
+                                    <div className="amount-tooltip">
+                                        <div className="amount-tooltip-text">
+                                            {Math.round(this.state.assetsValue).toLocaleString('en')}
+                                        </div>
                                     </div>
+                                }
+                                <div className="owned">
+                                    {dashboardAssets.count} <span>Owned</span>
                                 </div>
-                            }
-                            <div className="owned">
-                                {dashboardAssets.count} <span>Owned</span>
                             </div>
                         </Link>
-                    </div>
-                }
-                {
-                    !dashboardAssets &&
-                    <ContentLoader white noPaddingOnTheSides/>
-                }
+                    )}
+                    {
+                        !dashboardAssets &&
+                        <ContentLoader white noPaddingOnTheSides/>
+                    }
+                </div>
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    dashboardAssets : state.dashboard.dashboardAssets
+    dashboardAssets: state.dashboard.dashboardAssets
 })
 
 export default connect(mapStateToProps)(AssetsValue)
