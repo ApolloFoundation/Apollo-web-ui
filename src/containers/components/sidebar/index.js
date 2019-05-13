556472a1ae0f4581cb2276086a8d9d3f695775c6
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2019-05-13 16:47:24 +0300

    AWW-996 Remove unread messages icon

--- src/containers/components/sidebar/index.js
+++ src/containers/components/sidebar/index.js
@@ -453,16 +453,7 @@ class Sidebar extends React.Component {
 									className={`text ${this.getNavLinkClass(["/messenger"])}`}
 								>
 									Messages
-									{
-										this.props.notifications && this.props.notifications[1].subTypes[0].notificationCount === 0 &&
-										<i className="zmdi zmdi-comments left"/>
-									}
-									{
-										this.props.notifications && this.props.notifications[1].subTypes[0].notificationCount > 0 &&
-										<i className={`zmdi zmdi-comments left ${this.props.notifications[1].subTypes[0].notificationCount > 99 && 'big-count'}`}
-										   data-notification={this.props.notifications[1].subTypes[0].notificationCount}/>
-									}
-
+									<i className="zmdi zmdi-comments left"/>
 									<i className="zmdi zmdi-chevron-right right"/>
 								</NavLink>
 								<div
