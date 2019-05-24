99e28eb60bd0e39a320b9cb557f60b0a0241e557
DenisUAL <denisandreev64@gmail.com>
2019-05-24 16:14:59 +0300

    AWW-1074 about modal fix

--- src/containers/modals/about/index.js
+++ src/containers/modals/about/index.js
@@ -5,9 +5,9 @@
 
 
 import React from 'react';
-import {connect} from 'react-redux';
-import {setModalData}             from '../../../modules/modals';
-import {setBodyModalParamsAction} from "../../../modules/modals";
+import { connect } from 'react-redux';
+import { setModalData } from '../../../modules/modals';
+import { setBodyModalParamsAction } from "../../../modules/modals";
 import classNames from 'classnames';
 
 const mapStateToProps = state => ({
@@ -30,12 +30,13 @@ class AboutApollo extends React.Component {
     }
 
     render() {
-        const {blockchainStatus} = this.props;
+        const { blockchainStatus } = this.props;
 
         return (
             <div className="modal-box wide">
                 <div className="modal-form">
                     <div className="form-group-app devided about">
+                        <a onClick={() => this.props.closeModal()} className="exit"><i className="zmdi zmdi-close" /></a>
                         <div className="left-bar overflow-visible">
                             <div className="top-bar">
                                 <div className="form-title">
@@ -49,7 +50,6 @@ class AboutApollo extends React.Component {
                             </div>
                         </div>
                         <div className="right-bar">
-                            <a onClick={() => this.props.closeModal()} className="exit"><i className="zmdi zmdi-close" /></a>
                             <div className="description">
                                 Utilizing a team of world-class developers, managers, marketers, and researchers, the Apollo Foundation has set out to accomplish the vision of making Apollo the most technologically advanced, feature-rich, un-regulatable currency on Earth. We know the demands of a top-tier cryptocurrency, and we believe we can create a coin that will integrate everything necessary to rise above the rest. As we develop and improve Apollo, our team will strive to continue designing and developing features that have not previously been used in cryptocurrency. Our goal is to create the first all-in-one cryptocurrency, innovating and incorporating every ability that could be beneficial in currency, all in a single decentralized platform. Our first major update, Olympus Protocol, puts mass adoption-proof privacy at the core of Apollo. This is because we know the ability for a user to buy, sell, trade and send in absolute secrecy is vital to an industry that could be moments away from intense regulation. We are here because we believe that the only entity that should be in control of your funds is you.
 
@@ -58,13 +58,13 @@ class AboutApollo extends React.Component {
                                         <li><a target='blank' href='https://support.apollocurrency.com/support/home'><i class="zmdi zmdi-link" />&nbsp;Support</a></li>
                                     </ul>
                                 </p>
-                                
+
                                 <p>
-                                    <a target='blank' href='https://t.me/apollocommunity' className={'h2'} style={{color: '#0088cc'}}><i class="fab fa-telegram" /></a>      &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='blank' href='https://www.facebook.com/Apolloprivacycoin' className={'h2'} style={{color: '#3b5998'}}><i class="fab fa-facebook-f"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='blank' href='https://twitter.com/ApolloCurrency' className={'h2'} style={{color: '#1da1f2'}}><i class="fab fa-twitter"></i></a>     &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='blank' href='https://www.instagram.com/apollocurrency/?hl=en' className={'h2'} style={{color: '#c32aa3'}}><i class="fab fa-instagram"></i></a>   &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='blank' href='https://www.youtube.com/channel/UCZbB3PAUlkSKuBYEMG-l_CQ' className={'h2'} style={{color: '#ff0000'}}><i class="fab fa-youtube"></i></a>     &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='blank' href='https://t.me/apollocommunity' className={'h2'} style={{ color: '#0088cc' }}><i class="fab fa-telegram" /></a>      &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='blank' href='https://www.facebook.com/Apolloprivacycoin' className={'h2'} style={{ color: '#3b5998' }}><i class="fab fa-facebook-f"></i></a>  &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='blank' href='https://twitter.com/ApolloCurrency' className={'h2'} style={{ color: '#1da1f2' }}><i class="fab fa-twitter"></i></a>     &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='blank' href='https://www.instagram.com/apollocurrency/?hl=en' className={'h2'} style={{ color: '#c32aa3' }}><i class="fab fa-instagram"></i></a>   &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='blank' href='https://www.youtube.com/channel/UCZbB3PAUlkSKuBYEMG-l_CQ' className={'h2'} style={{ color: '#ff0000' }}><i class="fab fa-youtube"></i></a>     &nbsp;&nbsp;&nbsp;&nbsp;
                                 </p>
 
                                 <p>Copyright © 2019 Apollo Developers.</p>
@@ -73,7 +73,7 @@ class AboutApollo extends React.Component {
                                 <p>Copyright Web Wallet © 2019 Apollo Developers</p>
 
                                 <p>
-                                    Contact e-mail: <a href="">info@apollocurrency.com</a>
+                                    Contact e-mail: <a href="mailto:info@apollocurrency.com">info@apollocurrency.com</a>
                                 </p>
                                 <p>
                                     Actual Version: {blockchainStatus ? blockchainStatus.version : ''}
