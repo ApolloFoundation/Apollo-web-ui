75973b0c33492285c0cdfbb4669071bade116758
DenisUAL <denisandreev64@gmail.com>
2019-05-30 12:12:09 +0300

    AWW-1132 table fixes

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    server: 'http://51.15.235.41:7876',
+    server: '',
     localServerUrl: 'https://apollocurrency.com/api/client/twitter',
     mixerUrl: 'https://apollowallet.org/mixer/api/get-account',
     transportUrl: 'ws://127.0.0.1:8888/'
