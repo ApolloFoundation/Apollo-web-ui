01e95ad78ad789a14484c67f82f068d4f522c138
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2019-06-10 10:10:10 +0300

    AWW-1266 Create Backend Status Tasks page

---
+++ src/containers/account/backend-status/index.js
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Copyright © 2018 Apollo Foundation                                         *
+ *                                                                            *
+ ******************************************************************************/
+
+
+import React from 'react';
+import {connect} from 'react-redux';
+import Task from './task';
+import SiteHeader from '../../components/site-header';
+import CustomTable from '../../components/tables/table';
+
+
+const mapStateToProps = state => ({
+    blockchainStatus: state.account.blockchainStatus,
+});
+
+class BackendStatus extends React.Component {
+    render () {
+        const {blockchainStatus} = this.props;
+
+        return (
+            <div className="page-content">
+                <SiteHeader
+                    pageTitle={'Backend Status Tasks'}
+                />
+                <div className="page-body container-fluid">
+                    {blockchainStatus && blockchainStatus.status && (
+                        <CustomTable
+                            header={[
+                                {
+                                    name: 'Name',
+                                    alignRight: false
+                                },{
+                                    name: 'Description',
+                                    alignRight: false
+                                },{
+                                    name: 'Percent complete',
+                                    alignRight: false
+                                },{
+                                    name: 'State of task',
+                                    alignRight: false
+                                },{
+                                    name: 'Started',
+                                    alignRight: false
+                                },{
+                                    name: 'Finished',
+                                    alignRight: false
+                                },
+                            ]}
+                            TableRowComponent={Task}
+                            tableData={blockchainStatus.status.tasks}
+                            className={'no-min-height mb-3'}
+                            emptyMessage={'No tasks found.'}
+                        />
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps)(BackendStatus);
--- src/containers/components/site-header/settings/index.js
+++ src/containers/components/site-header/settings/index.js
@@ -18,6 +18,7 @@ const Settings = ({setBodyModalParamsAction, isLocalhost, isActive, closeMenu}) 
                 <li><NavLink activeClass={'active'} className="option" to="/blocks">Blocks</NavLink></li>
                 <li><NavLink activeClass={'active'} className="option" to="/peers">Peers</NavLink></li>
                 <li><NavLink activeClass={'active'} className="option" to="/generators">Generators</NavLink></li>
+                <li><NavLink activeClass={'active'} className="option" to="/backend-status">Backend Status Tasks</NavLink></li>
                 {
                     isLocalhost &&
                     <>
