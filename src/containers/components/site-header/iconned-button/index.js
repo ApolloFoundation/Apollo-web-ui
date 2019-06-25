5086d2f4967eb62d12b8ec91ab7a79c502210545
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2019-06-25 20:54:57 +0300

    AWW-1015 Add Support to header

--- src/containers/components/site-header/iconned-button/index.js
+++ src/containers/components/site-header/iconned-button/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-const IconndeButton = ({id, action, icon}) => (
-    <span
-        id={id}
-        className="header-action cursor-pointer"
-        onClick={action}
-    >
+const IconndeButton = ({id, action, icon, text, link, className}) => (
+    link ? (
+        <a
+            id={id}
+            className={`header-action cursor-pointer ${text ? 'header-action-text' : ''} ${className}`}
+            href={action}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+        >
+            {icon}
+            {text}
+        </a>
+    ) : (
+        <span
+            id={id}
+            className={`header-action cursor-pointer ${text ? 'header-action-text' : ''} ${className}`}
+            onClick={action}
+        >
         {icon}
+            {text}
     </span>
+    )
 );
 
 export default IconndeButton;
