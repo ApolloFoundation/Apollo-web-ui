5a40096fea44caba84ccd992309200e84467e23f
Igor Antonenko <antonenko.ihr@gmail.com>
2019-08-23 14:24:13 +0300

    APL-791 Added trade history page

---
+++ src/containers/modals/exchange/select-order/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ModalBody from '../../../components/modals/modal-body';
+import TextualInputComponent from '../../../components/form-components/textual-input';
+
+class SelectOrder extends React.Component {
+    render() {
+        return (
+            <ModalBody
+                loadForm={this.loadForm}
+                modalTitle={'Chosen trade'}
+                closeModal={this.props.closeModal}
+                isDisableSecretPhrase
+                nameModel={this.props.nameModal}
+            >
+                <TextualInputComponent
+                    field={'type'}
+                    disabled
+                    label={'Type'}
+                    placeholder={'Type'}
+                />
+                <TextualInputComponent
+                    field={'pairRate'}
+                    disabled
+                    label={'Price'}
+                    placeholder={'Price'}
+                />
+                <TextualInputComponent
+                    field={'offerAmount'}
+                    disabled
+                    label={'Amount'}
+                    placeholder={'Amount'}
+                />
+                <TextualInputComponent
+                    field={'total'}
+                    disabled
+                    label={'Total'}
+                    placeholder={'Total'}
+                />
+                <TextualInputComponent
+                    field={'statusName'}
+                    disabled
+                    label={'Status'}
+                    placeholder={'Status'}
+                />
+            </ModalBody>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    modalData: state.modals.modalData,
+});
+
+export default connect(mapStateToProps, null)(SelectOrder);
