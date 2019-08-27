561d7813ef28bd2f9d2376754a62985393421f90
Igor Antonenko <antonenko.ihr@gmail.com>
2019-08-27 11:03:43 +0300

    AWW-1276 delete url

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    server: 'http://51.15.114.68',
+    server: '',
     mixerUrl: 'https://apollowallet.org/mixer/api/get-account',
     transportUrl: 'ws://127.0.0.1:8888/',
     faucetUrl: 'https://wallet.test.apollowallet.org'
