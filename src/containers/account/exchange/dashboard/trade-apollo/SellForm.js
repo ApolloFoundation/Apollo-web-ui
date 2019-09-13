c312a4f8ab776b03e6039a7a403b0b4d0c3c762f
Igor Antonenko <igor.antonenko@firstbridge.io>
2019-09-13 14:57:55 +0300

    APL-863 Fixed pair name in table, fixed notification(APL-862)

--- src/containers/account/exchange/dashboard/trade-apollo/SellForm.js
+++ src/containers/account/exchange/dashboard/trade-apollo/SellForm.js
@@ -58,7 +58,6 @@ class SellForm extends React.PureComponent {
             const balanceFormat = balance ? (balance / ONE_APL) : 0;
             const { pairRate, offerAmount, total } = this.props.infoSelectedSellOrder;
             const { form, wallet } = this.state;
-            const rangeValue = (pairRate * offerAmount * 100).toFixed(0);
             form.setAllValues({
                 walletAddress: wallet[0],
                 pairRate: pairRate,
@@ -104,7 +103,7 @@ class SellForm extends React.PureComponent {
                         parseFloat(this.props.balanceAPL);
     
                     if (!this.props.balanceAPL || balanceAPL === 0 || balanceAPL < ((offerAmount + this.feeATM) / 10)) {
-                        NotificationManager.error('Not enough founds on your APL balance.', 'Error', 5000);
+                        NotificationManager.error('Not enough funds on your APL balance.', 'Error', 5000);
                         this.setPending(false);
                         return;
                     }
