e6c4d5b0e46e07719dcb97afa5c2bf425fc023be
Igor Antonenko <igor.antonenko@firstbridge.io>
2019-09-18 12:36:26 +0300

    APL-884 Reset form after rout

--- src/containers/account/exchange/dashboard/trade-apollo/index.js
+++ src/containers/account/exchange/dashboard/trade-apollo/index.js
@@ -14,6 +14,7 @@ class TradeApollo extends React.Component {
 
     componentDidMount() {
         this.getTransactionFee();
+        this.props.resetTrade();
     };
 
     getTransactionFee = async () => {
