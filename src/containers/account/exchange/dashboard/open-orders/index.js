be751b40b08faf786eb887a4692ab6f123937975
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2020-01-14 09:54:16 +0200

    APL-1188 Fix blocks on exchange dashboard

--- src/containers/account/exchange/dashboard/open-orders/index.js
+++ src/containers/account/exchange/dashboard/open-orders/index.js
@@ -26,70 +26,72 @@ class TradeHistoryExchange extends React.Component {
     render() {
         const {currentCurrency: {currency}, myOrders} = this.props;
         return (
-            <div className={'card card-light card-square bg-white'}>
-                <div className="card-body">
-                    <div className={'tabs-wrap tabs-primary mb-3'}>
-                        <div
-                            className={`tab-item ${this.state.actionType === 0 ? 'active' : ''}`}
-                            onClick={this.handleActionType.bind(this, 0)}
-                        >
-                            Open orders
+            <div className={'wrap-card-square'}>
+                <div className={'card card-light card-square bg-white'}>
+                    <div className="card-body">
+                        <div className={'tabs-wrap tabs-primary mb-3'}>
+                            <div
+                                className={`tab-item ${this.state.actionType === 0 ? 'active' : ''}`}
+                                onClick={this.handleActionType.bind(this, 0)}
+                            >
+                                Open orders
+                            </div>
+                            <div
+                                className={`tab-item ${this.state.actionType === 1 ? 'active' : ''}`}
+                                onClick={this.handleActionType.bind(this, 1)}
+                            >
+                                Closed orders
+                            </div>
                         </div>
-                        <div
-                            className={`tab-item ${this.state.actionType === 1 ? 'active' : ''}`}
-                            onClick={this.handleActionType.bind(this, 1)}
-                        >
-                            Closed orders
-                        </div>
+                        <CustomTable
+                            header={[
+                                {
+                                    name: `Price ${currency.toUpperCase()}`,
+                                    alignRight: false
+                                }, {
+                                    name: `Amount APL`,
+                                    alignRight: false
+                                }, {
+                                    name: `Total ${currency.toUpperCase()}`,
+                                    alignRight: false
+                                }, {
+                                    name: ``,
+                                    alignRight: true,
+                                    isRender: !(myOrders && myOrders.length > 0)
+                                }
+                            ]}
+                            className={'table-sm'}
+                            tableData={myOrders}
+                            emptyMessage={'No open orders found.'}
+                            TableRowComponent={(props) => {
+                                const pairRate = formatDivision(props.pairRate, ONE_GWEI, 9);
+                                const offerAmount = formatDivision(props.offerAmount, ONE_GWEI, 9);
+                                const total = formatDivision(props.pairRate * props.offerAmount, Math.pow(10, 18), 9);
+                                return (
+                                    <tr>
+                                        <td className={`${props.type === 0 ? 'text-success' : 'text-danger'}`}>{pairRate}</td>
+                                        <td>{offerAmount}</td>
+                                        <td>{total}</td>
+                                        <td className={'align-right'}>
+                                            <button
+                                                type={'button'}
+                                                className="btn btn-sm p-0 btn-icon"
+                                                onClick={() => this.handleCancel({
+                                                    currency,
+                                                    pairRate,
+                                                    offerAmount,
+                                                    total,
+                                                    orderId: props.id,
+                                                })}
+                                            >
+                                                <i className="zmdi zmdi-close"/>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            }}
+                        />
                     </div>
-                    <CustomTable
-                        header={[
-                            {
-                                name: `Price ${currency.toUpperCase()}`,
-                                alignRight: false
-                            }, {
-                                name: `Amount APL`,
-                                alignRight: false
-                            }, {
-                                name: `Total ${currency.toUpperCase()}`,
-                                alignRight: false
-                            }, {
-                                name: ``,
-                                alignRight: true,
-                                isRender: !(myOrders && myOrders.length > 0)
-                            }
-                        ]}
-                        className={'table-sm'}
-                        tableData={myOrders}
-                        emptyMessage={'No open orders found.'}
-                        TableRowComponent={(props) => {
-                            const pairRate = formatDivision(props.pairRate, ONE_GWEI, 9);
-                            const offerAmount = formatDivision(props.offerAmount, ONE_GWEI, 9);
-                            const total = formatDivision(props.pairRate * props.offerAmount, Math.pow(10, 18), 9);
-                            return (
-                                <tr>
-                                    <td className={`${props.type === 0 ? 'text-success' : 'text-danger'}`}>{pairRate}</td>
-                                    <td>{offerAmount}</td>
-                                    <td>{total}</td>
-                                    <td className={'align-right'}>
-                                        <button
-                                            type={'button'}
-                                            className="btn btn-sm p-0 btn-icon"
-                                            onClick={() => this.handleCancel({
-                                                currency,
-                                                pairRate,
-                                                offerAmount,
-                                                total,
-                                                orderId: props.id,
-                                            })}
-                                        >
-                                            <i className="zmdi zmdi-close"/>
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        }}
-                    />
                 </div>
             </div>
         );
