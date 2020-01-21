c51add4ede4036ae760df96b37c3e59e82c0ab25
Igor Antonenko <igor.antonenko@firstbridge.io>
2020-01-21 11:55:27 +0200

    APL-1244_Fix_word Fixed word and delete TV actions

--- src/containers/components/simple-progress-bar/simple-progress-bar.js
+++ src/containers/components/simple-progress-bar/simple-progress-bar.js
@@ -8,7 +8,7 @@ const SimpleProgressBar = ({step, time, blockTime, status}) => {
         0: 'The first user send a contract, wait for review and approval. (On this step user doesn`t transfer money.)',
         1: 'The second user approve the contract, transfer the money and wait for counter transfer.',
         2: 'The first user send counter transfer.',
-        3: 'Cansel',
+        3: 'Cancel',
     }
 
     const listClosedStatuses = {
