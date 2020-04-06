0333113914a53e8a5c33cc8ed518accf747cf7fc
Igor Antonenko <igor.antonenko@firstbridge.io>
2020-04-06 13:13:23 +0300

    APL-Fix_mixer Fixed mixer

--- src/config.js
+++ src/config.js
@@ -9,7 +9,6 @@ const config = {};
 // API Gateway
 config.api = {
     server: '',
-    mixerUrl: 'http://51.15.228.126:8080/api/get-account',
     transportUrl: 'ws://127.0.0.1:8888/',
     faucetUrl: 'https://wallet.test.apollowallet.org'
 };
