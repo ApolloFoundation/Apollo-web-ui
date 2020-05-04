9fed16d847927dbfb5f24398f28b32eb860e13a2
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2020-05-04 22:03:43 +0300

    Revert config

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    server: 'https://apl-t3-3.testnet3.apollowallet.org',
+    server: '',
     transportUrl: 'ws://127.0.0.1:8888/',
     faucetUrl: 'https://wallet.test.apollowallet.org'
 };
