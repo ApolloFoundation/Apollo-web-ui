ddbc3f63a4469127faebcd2653609892e8662ae4
antonenko.igor <igor.antonenko@firstbridge.io>
2020-07-07 22:34:32 +0300

    APL-1579_Update_account_properties Updated acc properties

--- src/containers/account/account-properties/index1.jsx
+++ src/containers/account/account-properties/index1.jsx
@@ -3,73 +3,71 @@
  *                                                                            *
  ***************************************************************************** */
 
-import React, { useEffect, useCallback } from 'react';
-import { useState, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import SiteHeader from '../../components/site-header';
+import React, {
+  useState, useEffect, useCallback,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAccountPropertiesAction } from '../../../actions/account/index';
-import InfoBox from '../../components/info-box';
 import { setBodyModalParamsAction } from '../../../modules/modals';
-import ContentLoader from '../../components/content-loader';
-import ContentHendler from '../../components/content-hendler';
-
-import AccountProperty from './acocunt-property';
+import AccountProperty from './acocunt-property/index1';
+import SiteHeader from '../../components/site-header';
 import CustomTable from '../../components/tables/table';
+import Button from '../../components/button';
+
+const initialPagination = {
+  page: 1,
+  firstIndex: 0,
+  lastIndex: 15,
+};
 
 export default function AccountProperties(props) {
   const dispatch = useDispatch();
 
+  const { account } = useSelector(state => state.account);
+
   const [properties, setProperties] = useState(null);
-  const [recipientRS, setRecipientRS] = useState(null);
-  const [setterRS, setSetterRS] = useState(null);
   const [incoming, setIncoming] = useState(true);
-  const [pagination, setPagination] = useState({
-    page: 1,
-    firstIndex: 0,
-    lastIndex: 15,
-  });
+  const [pagination, setPagination] = useState({ ...initialPagination });
 
-  const getAccountPropertiesIncoming = useCallback(async (state, currPagination) => {
+  const getAccountPropertiesIncoming = useCallback(async (currAccount, currPagination) => {
     let newPagination = currPagination;
-    let newState = state;
+    let newAccount = currAccount;
 
-    if (!newState) newState = props;
+    if (!newAccount) newAccount = account;
     if (!newPagination) {
       newPagination = { ...pagination };
     }
 
     const newProperties = await dispatch(getAccountPropertiesAction({
-      recipient: newState.account,
+      recipient: newAccount,
       firstIndex: newPagination.firstIndex,
       lastIndex: newPagination.lastIndex,
     }));
 
     if (newProperties) {
       setProperties(newProperties.properties);
-      setRecipientRS(newProperties.recipientRS);
       setIncoming(true);
-      setPagination(...newPagination);
+      setPagination({ ...newPagination });
     }
   }, [dispatch, pagination, props]);
 
-  const getAccountPropertiesOutgoing = useCallback(async (state, currPagination) => {
+  const getAccountPropertiesOutgoing = useCallback(async (currAccount, currPagination) => {
     let newPagination = currPagination;
-    let newState = state;
+    let newAccount = currAccount;
 
-    if (!newState) newState = props;
+    if (!newAccount) newAccount = account;
     if (!newPagination) {
       newPagination = { ...pagination };
     }
 
     const newProperties = await dispatch(getAccountPropertiesAction({
-      setter: newState.account,
+      setter: newAccount,
       firstIndex: newPagination.firstIndex,
       lastIndex: newPagination.lastIndex,
     }));
 
     if (newProperties) {
       setProperties(newProperties.properties);
-      setSetterRS(newProperties.setterRS);
       setIncoming(false);
       setPagination(...newPagination);
     }
@@ -89,9 +87,66 @@ export default function AccountProperties(props) {
   }, [getAccountPropertiesIncoming]);
 
   useEffect(() => {
-    getAccountPropertiesIncoming();
+    if (incoming) getAccountPropertiesIncoming(account);
+    else getAccountPropertiesOutgoing(account);
+  }, [incoming, account]);
 
-    if (incoming) getAccountPropertiesIncoming(props);
-    else getAccountPropertiesOutgoing(props);
-  }, [getAccountPropertiesIncoming, getAccountPropertiesOutgoing, incoming, props]);
+  useEffect(() => {
+    getAccountPropertiesIncoming();
+  }, []);
+
+  return (
+    <div className="page-content">
+      <SiteHeader
+        pageTitle={'Account properties'}
+      >
+        <a
+          className={`btn ${incoming ? 'outline-primary' : 'outline-transparent'} mr-1`}
+          onClick={() => getAccountPropertiesIncoming(null, initialPagination)}
+        >
+          Incoming
+        </a>
+        <a
+          className={`btn ${incoming ? 'outline-transparent' : 'outline-primary'} mr-1`}
+          onClick={() => getAccountPropertiesOutgoing(null, initialPagination)}
+        >
+          Outgoing
+        </a>
+        <Button
+          color="green"
+          size="sm"
+          name="Set"
+          onClick={setProperty}
+        />
+      </SiteHeader>
+      <div className="page-body container-fluid">
+        <CustomTable
+          header={[
+            {
+              name: `${incoming ? 'Setter' : 'Recipient'}`,
+              alignRight: false
+            },{
+              name: 'Property',
+              alignRight: false
+            },{
+              name: 'Value',
+              alignRight: false
+            },{
+              name: 'Actions',
+              alignRight: true
+            }
+          ]}
+          className={'mb-3'}
+          emptyMessage={'No account properties found .'}
+          TableRowComponent={conponentProps => <AccountProperty incoming={incoming} {...conponentProps}/>}
+          tableData={properties}
+          isPaginate
+          page={pagination.page}
+          previousHendler={() => onPaginate(pagination.page - 1)}
+          nextHendler={() => onPaginate(pagination.page + 1)}
+          itemsPerPage={15}
+        />
+      </div>
+    </div>
+  );
 }
