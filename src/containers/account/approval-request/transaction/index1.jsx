1a5b1926bc91560e681ae520193076a5d0eb9f01
antonenko.igor <igor.antonenko@firstbridge.io>
2020-07-10 15:29:16 +0300

    APL-1590_Update_Approval_requests fixed tr-ns in approval rq-s

--- src/containers/account/approval-request/transaction/index1.jsx
+++ src/containers/account/approval-request/transaction/index1.jsx
@@ -1,69 +1,67 @@
-/******************************************************************************
+/** ****************************************************************************
  * Copyright © 2018 Apollo Foundation                                         *
  *                                                                            *
- ******************************************************************************/
-
+ ***************************************************************************** */
 
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import {setBodyModalParamsAction} from "../../../../modules/modals";
-import {formatTimestamp} from "../../../../helpers/util/time";
-import CryptoJS from 'crypto-js'
-import {getTransactionAction} from "../../../../actions/transactions";
-import {ONE_APL} from '../../../../constants';
+import CryptoJS from 'crypto-js';
+import { setBodyModalParamsAction } from '../../../../modules/modals';
+import { formatTimestamp } from '../../../../helpers/util/time';
+import { getTransactionAction } from '../../../../actions/transactions';
+import { ONE_APL } from '../../../../constants';
 
-class Transaction extends React.Component {
-    getTransactionInfo = async transaction => {
-        return await this.props.getTransactionAction({
-            transaction,
-            random: Math.random()
-        })
-    };
+export default function Transaction(props) {
+  const dispatch = useDispatch();
 
-    render () {
-        const {transaction, timestamp, formatTimestamp, amountATM, feeATM, senderRS, attachment, height, confirmations, setBodyModalParamsAction} = this.props;
+  const {
+    transaction, timestamp, amountATM, confirmations,
+    feeATM, senderRS, attachment, height,
+  } = props;
 
-        return (
-            <tr key={uuidv4()}>
-                <td className="blue-link-text">
-                    <a onClick={async () => setBodyModalParamsAction('INFO_TRANSACTION', await this.getTransactionInfo(transaction))}>
-                        {formatTimestamp(timestamp)}
-                    </a>
-                </td>
-                <td className="align-right">Ordinary Payment</td>
-                <td className="align-right">{amountATM / ONE_APL}</td>
-                <td>{feeATM / ONE_APL}</td>
-                <td className="blue-link-text align-right"><a onClick={setBodyModalParamsAction.bind(this, 'INFO_ACCOUNT', senderRS)}>{senderRS}</a></td>
-                <td>{attachment.phasingHolding} / {attachment.phasingQuorum}</td>
-                <td>{height}</td>
-                <td>{confirmations}</td>
-                <td className="">
-                    <div className="btn-box inline">
-                        <button
-                            type={'button'}
-                            onClick={() => {
-                                setBodyModalParamsAction("APPROVE_TRANSACTION", {transaction: this.props});
-                            }}
-                            className="btn btn-default"
-                        >
-                            Approve
-                        </button>
-                    </div>
-                </td>
-            </tr>
-        );
-    }
+  const getTransactionInfo = useCallback(async transaction => await dispatch(getTransactionAction({
+    transaction,
+    random: Math.random(),
+  })), [dispatch]);
+
+  return (
+    <tr key={uuidv4()}>
+      <td className="blue-link-text">
+        <a onClick={async () => dispatch(setBodyModalParamsAction('INFO_TRANSACTION', await this.getTransactionInfo(transaction)))}>
+          {dispatch(formatTimestamp(timestamp))}
+        </a>
+      </td>
+      <td className="align-right">Ordinary Payment</td>
+      <td className="align-right">{amountATM / ONE_APL}</td>
+      <td>{feeATM / ONE_APL}</td>
+      <td className="blue-link-text align-right">
+        <a onClick={dispatch(setBodyModalParamsAction('INFO_ACCOUNT', senderRS))}>
+          {senderRS}
+        </a>
+      </td>
+      <td>
+        {attachment.phasingHolding}
+        {' '}
+        /
+        {' '}
+        {attachment.phasingQuorum}
+      </td>
+      <td>{height}</td>
+      <td>{confirmations}</td>
+      <td className="">
+        <div className="btn-box inline">
+          <button
+            type="button"
+            onClick={() => {
+              setBodyModalParamsAction('APPROVE_TRANSACTION', { transaction: this.props });
+            }}
+            className="btn btn-default"
+          >
+            Approve
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
 }
-
-const mapDispatchToProps = dispatch => ({
-    formatTimestamp: (timestamp, date_only, isAbsoluteTime) => dispatch(formatTimestamp(timestamp, date_only, isAbsoluteTime)),
-    setBodyModalParamsAction: (type, data, valueForModal) => dispatch(setBodyModalParamsAction(type, data, valueForModal)),
-    getTransactionAction: (reqParams) => dispatch(getTransactionAction(reqParams)),
-});
-
-const mapStateToProps = state => ({
-   account: state.account.account
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Transaction);
