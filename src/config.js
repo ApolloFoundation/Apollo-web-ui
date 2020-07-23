c97fe936e1d22608caaa32c001dfc3cc84693b93
Antonenko Igor <igor.antonenko@firstbridge.io>
2020-07-23 17:52:47 +0300

    APL-1528_Update_WW fix config

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-  server: 'http://127.0.0.1:7876',
+  server: '',
   transportUrl: 'ws://127.0.0.1:8888/',
   faucetUrl: 'https://wallet.test.apollowallet.org',
 };
