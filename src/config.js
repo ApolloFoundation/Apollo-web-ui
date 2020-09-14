66ce8d7053fc116c1c468e1228641e244afc7ac8
Antonenko Igor <igor.antonenko@firstbridge.io>
2020-09-14 17:14:41 +0300

    APL-1696_Fix_name_of_coin fixed name for dashboard and account ladger

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-  server: '',
+  server: 'https://apl-tap-0.testnet-ap.apollowallet.org',
   transportUrl: 'ws://127.0.0.1:8888/',
   faucetUrl: 'https://wallet.test.apollowallet.org',
 };
