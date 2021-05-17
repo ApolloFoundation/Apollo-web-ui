e6dfbb576abaa1b613228777c7f4fcf22075f75b
serhii skachkov <serhii.skachkov@firstbridge.io>
2021-05-17 17:12:11 +0300

    apollo-smc-fix errors

--- src/containers/account/smart-contracts/index.js
+++ src/containers/account/smart-contracts/index.js
@@ -67,7 +67,6 @@ class SmartContracts extends React.Component {
   };
 
   render() {
-    console.log(this.state.contracts);
     return (
       <div className="page-content">
         <SiteHeader pageTitle={"Smart Contracts"}>
