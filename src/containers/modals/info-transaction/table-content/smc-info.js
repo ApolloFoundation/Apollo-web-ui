e1c637a8d8e38e068da23bc5462dd8be25b579e5
serhii skachkov <serhii.skachkov@firstbridge.io>
2021-06-04 16:46:31 +0300

    AD-230_add_smc_transaction_info Add new feature

---
+++ src/containers/modals/info-transaction/table-content/smc-info.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+
+export default class SmcInfo extends Component {
+    render() {
+        return(
+            <React.Fragment>
+	            {this.props.transaction.amountATM &&
+	            <tr>
+		            <td>Amount:</td>
+		            <td>{new Number(this.props.transaction.amountATM / this.props.decimals).toFixed(8)}</td>
+	            </tr>
+	            }
+	            {this.props.transaction.feeATM &&
+	            <tr>
+		            <td>Fee:</td>
+		            <td>{this.props.transaction.feeATM / this.props.decimals}</td>
+	            </tr>
+	            }
+	            {this.props.transaction.senderRS &&
+	            <tr>
+		            <td>From:</td>
+		            <td>{this.props.transaction.senderRS}</td>
+	            </tr>
+	            }
+	            {this.props.transaction.recipientRS &&
+	            <tr>
+		            <td>To:</td>
+		            <td>{this.props.transaction.recipientRS}</td>
+	            </tr>
+	            }
+	            {this.props.transaction.fullHash &&
+	            <tr>
+		            <td>Hash:</td>
+		            <td>{this.props.transaction.fullHash}</td>
+	            </tr>
+	            }
+            </React.Fragment>
+        );
+    }
+}
