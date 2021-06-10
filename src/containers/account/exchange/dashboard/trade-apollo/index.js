a7c5d92ad4ccd31916d861150348093a1ebcd04e
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2021-06-11 01:30:09 +0500

    fix pollo trade to the old version

--- src/containers/account/exchange/dashboard/trade-apollo/index.js
+++ src/containers/account/exchange/dashboard/trade-apollo/index.js
@@ -1,90 +1,86 @@
-import React, {
-    useState, useCallback, useEffect,
-  } from 'react';
-  import { useSelector, connect } from 'react-redux';
-  import { getTransactionFee } from '../../../../../actions/wallet';
-  import { setTypeOfTrade, resetTrade } from '../../../../../modules/modals';
-  import BuyForm from './buy-form';
-  import SellForm from './sell-form';
-  import store from '../../../../../store';
-  
-  const feeATM = 200000000;
-  const ethMinTxFee = 0.002;
-  
-  export default function TradeApollo(props) {
-    const { dispatch } = store;
-  
-    const { typeOfTrade } = useSelector(state => state.modals);
-    const { decimals } = useSelector(state => state.account);
-  
-    const { wallet, handleLoginModal, ticker } = props;
-  
-    const [maxFee, setMaxFee] = useState();
-  
-    const handleGetTransactionFee = useCallback(async () => {
-      const transactionFee = await dispatch(getTransactionFee());
-      if (transactionFee) {
-        setMaxFee(transactionFee.fast);
-      }
-    }, [dispatch]);
-  
-    const changeTabOfTrade = useCallback(currTypeOfTrade => {
-      dispatch(setTypeOfTrade(currTypeOfTrade));
-      dispatch(resetTrade());
-    }, [dispatch]);
-  
-    useEffect(() => {
-      handleGetTransactionFee();
-      dispatch(resetTrade());
-    }, [dispatch, handleGetTransactionFee]);
-  
-    return (
-      <div className="card card-light h-400">
-        <div className="card-title">
-          <div className="title">Trade Apollo</div>
-          <span className="sub-title">
-            Fee:
-            {feeATM / decimals}
-            {' '}
-            {ticker}
-          </span>
-        </div>
-        <div className="card-body">
-          <div className="tabs-wrap mb-3">
-            <div
-              className={`tab-item ${typeOfTrade === 'BUY' ? 'active' : ''}`}
-              onClick={() => changeTabOfTrade('BUY')}
-            >
-              Buy
-              {' '}
-              {ticker}
+import React from 'react';
+import {connect} from "react-redux";
+import {ONE_APL} from '../../../../../constants';
+import {getTransactionFee} from "../../../../../actions/wallet";
+import {setTypeOfTrade, resetTrade} from "../../../../../modules/modals";
+import BuyForm from "./BuyForm";
+import SellForm from "./SellForm";
+import {formatGweiToEth} from "../../../../../helpers/format";
+
+class TradeApollo extends React.Component {
+    feeATM = 200000000;
+    state = {
+        maxFee: null,
+    };
+
+    componentDidMount() {
+        this.getTransactionFee();
+        this.props.resetTrade();
+    };
+
+    getTransactionFee = async () => {
+        const transactionFee = await this.props.getTransactionFee();
+        if (transactionFee) {
+            this.setState({maxFee: transactionFee.fast});
+        }
+    };
+
+    changeTabOfTrade = typeOfTrade => {
+        this.props.setTypeOfTrade(typeOfTrade);
+        this.props.resetTrade();
+    };
+
+    render() {
+        const {wallet, handleLoginModal, typeOfTrade} = this.props;
+        const ethMinTxFee = 0.002;
+        return (
+            <div className={'card card-light h-400'}>
+                <div className="card-title">
+                    <div className={'title'}>Trade Apollo</div>
+                    <span className={'sub-title'}>Fee: {this.feeATM / ONE_APL} APL</span>
+                </div>
+                <div className="card-body">
+                    <div className={'tabs-wrap mb-3'}>
+                        <div
+                            className={`tab-item ${typeOfTrade === 'BUY' ? 'active' : ''}`}
+                            onClick={() => this.changeTabOfTrade('BUY')}
+                        >
+                            Buy APL
+                        </div>
+                        <div
+                            className={`tab-item ${typeOfTrade === 'SELL' ? 'active' : ''}`}
+                            onClick={() => this.changeTabOfTrade('SELL')}
+                        >
+                            Sell APL
+                        </div>
+                    </div>
+                    {typeOfTrade === 'BUY' ? (
+                        <BuyForm
+                            wallet={wallet}
+                            handleLoginModal={handleLoginModal}
+                            ethFee={ethMinTxFee}
+                        />
+                    ) : (
+                        <SellForm
+                            wallet={wallet}
+                            handleLoginModal={handleLoginModal}
+                            ethFee={ethMinTxFee}
+                        />
+                    )}
+                </div>
             </div>
-            <div
-              className={`tab-item ${typeOfTrade === 'SELL' ? 'active' : ''}`}
-              onClick={() => changeTabOfTrade('SELL')}
-            >
-              Sell
-              {' '}
-              {ticker}
-            </div>
-          </div>
-          {typeOfTrade === 'BUY' ? (
-            <BuyForm
-              ticker={ticker}
-              wallet={wallet}
-              handleLoginModal={handleLoginModal}
-              ethFee={ethMinTxFee}
-            />
-          ) : (
-            <SellForm
-              ticker={ticker}
-              decimals={decimals}
-              wallet={wallet}
-              handleLoginModal={handleLoginModal}
-              ethFee={ethMinTxFee}
-            />
-          )}
-        </div>
-      </div>
-    );
-  }
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    typeOfTrade: state.modals.typeOfTrade,
+});
+
+const mapDispatchToProps = dispatch => ({
+    resetTrade: () => dispatch(resetTrade()),
+    getTransactionFee: () => dispatch(getTransactionFee()),
+    setTypeOfTrade: (params) => dispatch(setTypeOfTrade(params)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TradeApollo);
