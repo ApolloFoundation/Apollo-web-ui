ea8515c39714226b1e034746f4e82b4399cc9a9b
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2021-06-29 18:47:18 +0500

    clean up

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-    server: 'http://51.15.37.165:7876',
+    server: '',
     transportUrl: 'ws://127.0.0.1:8888/',
     faucetUrl: 'https://wallet.test.apollowallet.org'
 };
