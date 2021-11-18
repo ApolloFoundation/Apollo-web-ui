0c02d57ad697dbd71e86a72cc3ebf3bfcd32e876
sergeyskachkov-cpu <serhii.skachkov@firstbridge.io>
2021-11-18 12:08:34 +0200

    feature/AD-385_fix_crush_read_method update panel method

--- src/containers/account/smart-contracts/panels/panel-method.jsx
+++ src/containers/account/smart-contracts/panels/panel-method.jsx
@@ -60,7 +60,6 @@ const PanelMethod = ({ items, address, type, title, token }) => {
               <>
                 <span>
                   {item.outputs[0].type === "uint" && !(item.name === "releaseTime" || item.name === "decimals") ? (
-                    <>
                       <div className="mb-1">
                         <span className="text-info">{convertToToken(item.value, 8, true)} </span>
                         {token.value} (
@@ -69,7 +68,6 @@ const PanelMethod = ({ items, address, type, title, token }) => {
                           </span>
                         )
                       </div>
-                    </>
                   ) : (
                     <>
                       {item.value}
