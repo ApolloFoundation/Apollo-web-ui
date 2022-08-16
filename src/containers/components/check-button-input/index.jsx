99b0e40d97dbb5390294914bb0342b1f06b070dd
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-08-16 13:07:00 +0300

    APOLLO-2158: remove private label from private transactiom modal, it move to the common transaction modal and checkbox input edit for available to be a controll input

--- src/containers/components/check-button-input/index.jsx
+++ src/containers/components/check-button-input/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useField } from 'formik';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
@@ -6,7 +6,7 @@ import './styles.scss';
 
 const CheckboxFormInput = props => {
   const {
-    label, name, id, className, disabled, onChange,
+    label, name, id, className, disabled, onChange, value,
   } = props;
 
   const [field, , helpers] = useField({
@@ -19,6 +19,12 @@ const CheckboxFormInput = props => {
     setValue(checked);
     if (onChange) onChange();
   }, [onChange, setValue]);
+
+  useEffect(() => {
+    if (typeof value !== undefined) {
+      helpers.setValue(value);
+    }
+  }, [value]);
 
   return (
     <div className={cn('checkbox-group mb-15', className)}>
---
+++ src/containers/modals/send-apollo/PrivateTransactionConfirm/PrivateTransactionConfirm.jsx
@@ -0,0 +1,41 @@
+/** ****************************************************************************
+ * Copyright © 2018 Apollo Foundation                                         *
+ *                                                                            *
+ ***************************************************************************** */
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import InfoBox from '../../../components/info-box';
+import Button from '../../../components/button';
+import { setBodyModalParamsAction } from '../../../../modules/modals';
+import styles from './index.module.scss';
+
+// values prop goes from ModalBody by default
+export const PrivateTransactionConfirm = ({ onClose, values }) => {
+  const dispatch = useDispatch();
+
+  const handleAgree = () => {
+    const { privateTransaction, ...formValues } = values;
+    dispatch(setBodyModalParamsAction('SEND_APOLLO_PRIVATE', { ...formValues, feeATM: 5 }))
+  };
+
+  return (
+    <div className={styles.privateTransactionConfirmWrapper}>
+      <InfoBox info className={styles.privateTransactionConfirmIfoBox}>
+      <button type="button" onClick={onClose} className="exit">
+        <i className="zmdi zmdi-close" />
+      </button>
+        Please note: Exchanges may not support private transactions, we recommend sending publically to exchanges.
+        <br />
+        Private transactions currently protect down the API level. Database level protection will start with Olympus 2.0
+        <br />
+        <div className={styles.privateTransactionConfirmButtonWrapper}>
+          <Button
+            className="mt-3"
+            name="I agree"
+            onClick={handleAgree}
+          />
+        </div>
+      </InfoBox>
+    </div>
+  )
+}
