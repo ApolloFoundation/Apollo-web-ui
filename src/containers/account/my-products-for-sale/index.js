e23aa3e3a1a70bd95ba54f3db97ebc975d605f63
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-09-15 17:38:12 +0400

    refactor my-product-for-sale component

--- src/containers/account/my-products-for-sale/index.js
+++ src/containers/account/my-products-for-sale/index.js
@@ -4,149 +4,109 @@
  ******************************************************************************/
 
 
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import MarketplaceTableItem from '../marketplace/marketplace-table-item';
 import SiteHeader from  '../../components/site-header'
 import {setBodyModalParamsAction } from "../../../modules/modals";
 import {BlockUpdater} from "../../block-subscriber/index";
-
 import CustomTable from '../../components/tables/table';
-
 import {getDGSGoodsAction} from "../../../actions/marketplace";
 
-class MyProductsForSale extends React.Component {
-    constructor(props) {
-        super(props);
-
-
-        this.state = {
+const MyProductsForSale = () => {
+    const dispatch = useDispatch();
+    const account = useSelector(state => state.account.account);
+    const [state, setState] = useState({
             page: 1,
             firstIndex: 0,
             lastIndex: 15,
             getDGSGoods: []
-        };
-    }
-
-    componentDidMount() {
-        this.getDGSGoods({
-            seller: this.props.account,
-            firstIndex: this.state.firstIndex,
-            lastIndex: this.state.lastIndex
         });
-        BlockUpdater.on("data", data => {
-            console.warn("height in dashboard", data);
-            console.warn("updating dashboard");
-            this.getDGSGoods({
-                seller: this.props.account,
-                firstIndex: this.state.firstIndex,
-                lastIndex: this.state.lastIndex
-            });
-        });
-    }
 
-    componentWillUnmount() {
-        BlockUpdater.removeAllListeners('data');
-    }
+    const getDGSGoods = useCallback(async () => {
+        const { goods } = await dispatch(getDGSGoodsAction({
+            seller: account,
+            firstIndex: state.firstIndex,
+            lastIndex: state.lastIndex,
+        }));
 
-    componentWillReceiveProps(newState) {
-        this.setState({
-            ...newState
-        }, () => {
-            this.getDGSGoods({
-                PublicKey: this.state.publicKey,
-                seller: this.props.account,
-                firstIndex: this.state.firstIndex,
-                lastIndex: this.state.lastIndex
-            });
-        });
-    }
+        if (goods) {
+            setState(prevState => ({
+                ...prevState,
+                getDGSGoods: goods,
+            }))
+        }
+    }, [dispatch, account, state.firstIndex, state.lastIndex]);
 
-    onPaginate (page) {
+    const updater = useCallback(() => getDGSGoods(), [getDGSGoods]);
+
+    const handleListProduct = () => dispatch(setBodyModalParamsAction('LIST_PRODUCT_FOR_SALE', {}));
+
+    const handlePaginate = (page) => () => {
         let reqParams = {
-            page: page,
-            account: this.props.account,
+            page,
             firstIndex: page * 15 - 15,
             lastIndex:  page * 15
         };
 
-        this.setState(reqParams, () => {
-            this.getDGSGoods(reqParams)
-        });
+        setState(prevState => ({
+            ...prevState,
+            ...reqParams,
+        }));
     }
 
-    getDGSGoods = async (reqParams) => {
-        const getDGSGoods = await this.props.getDGSGoodsAction(reqParams);
-
-        if (getDGSGoods) {
-            this.setState({
-                ...this.state,
-                getDGSGoods: getDGSGoods.goods
-            })
+    useEffect(() => {
+        getDGSGoods();
+        BlockUpdater.on("data", updater);
+        return () => {
+            BlockUpdater.removeAllListeners('data', updater);
         }
-    };
+    }, [getDGSGoods, updater]);
 
-    render () {
 
-        return (
-            <div className="page-content">
-                <SiteHeader
-                    pageTitle={'My products for sale'}
+    return (
+        <div className="page-content">
+            <SiteHeader
+                pageTitle='My products for sale'
+            >
+                <button
+                    type='button'
+                    className="btn btn-green btn-sm"
+                    onClick={handleListProduct}
                 >
-                    <button
-                        type={'button'}
-                        className="btn btn-green btn-sm"
-                        onClick={() => this.props.setBodyModalParamsAction('LIST_PRODUCT_FOR_SALE', {})}
-                    >
-                        List product for sale
-                    </button>
-                </SiteHeader>
-                <div className="page-body container-fluid">
-                    <CustomTable 
-                        header={[
-                            {
-                                name: 'Name',
-                                alignRight: false
-                            },{
-                                name: 'Quantity',
-                                alignRight: true
-                            },{
-                                name: 'Price',
-                                alignRight: true
-                            },{
-                                name: 'Actions',
-                                alignRight: true
-                            }
-                        ]}
-                        emptyMessage={'No products found.'}
-                        page={this.state.page}
-                        className={'mb-3'}
-                        TableRowComponent={MarketplaceTableItem}
-                        tableData={this.state.getDGSGoods}
-                        isPaginate
-                        previousHendler={this.onPaginate.bind(this, this.state.page - 1)}
-                        nextHendler={this.onPaginate.bind(this, this.state.page + 1)}
-                        itemsPerPage={15}
-                    />
-                </div>
+                    List product for sale
+                </button>
+            </SiteHeader>
+            <div className="page-body container-fluid">
+                <CustomTable 
+                    header={[
+                        {
+                            name: 'Name',
+                            alignRight: false
+                        },{
+                            name: 'Quantity',
+                            alignRight: true
+                        },{
+                            name: 'Price',
+                            alignRight: true
+                        },{
+                            name: 'Actions',
+                            alignRight: true
+                        }
+                    ]}
+                    emptyMessage='No products found.'
+                    page={state.page}
+                    className='mb-3'
+                    TableRowComponent={MarketplaceTableItem}
+                    tableData={state.getDGSGoods}
+                    isPaginate
+                    previousHendler={handlePaginate(state.page - 1)}
+                    nextHendler={handlePaginate(state.page + 1)}
+                    itemsPerPage={15}
+                />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-const mapStateToProps = state => ({
-    account: state.account.account,
-
-    // modals
-    modalData: state.modals.modalData
-});
-
-const initMapDispatchToProps = dispatch => ({
-    getDGSGoodsAction: (reqParams) => dispatch(getDGSGoodsAction(reqParams)),
-    setBodyModalParamsAction: (type, data, valueForModal) => dispatch(setBodyModalParamsAction(type, data, valueForModal)),
-});
-
-export default connect(
-    mapStateToProps,
-    initMapDispatchToProps
-)(MyProductsForSale);
+export default MyProductsForSale;
