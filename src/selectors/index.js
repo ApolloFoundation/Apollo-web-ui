0b2e51376f239772ee82699dcd3238e88071179f
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-09-16 14:21:43 +0400

    part of the settings component

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,3 +3,6 @@ export const getPassPhraseSelector = state => state.account.passPhrase;
 export const getMessagesSelector = state => state.messages.messages;
 export const getDecimalsSelector = state => state.account.decimals;
 export const getTickerSelector = state => state.account.ticker;
+export const getSettingsSelector = state => state.accountSettings;
+export const get2FASelector = state => state.account.is2FA;
+export const getIsLocalhostSelector = state => state.account.isLocalhost;
