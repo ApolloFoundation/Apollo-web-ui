12e10a0e7e769d6c7205f1e09d966e915718f93c
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-10-05 17:40:38 +0400

    refactor transfer history

--- src/containers/account/transfer-history/index.jsx
+++ src/containers/account/transfer-history/index.jsx
@@ -3,66 +3,34 @@
  *                                                                            *
  ***************************************************************************** */
 
-import React, {
-  useEffect, useCallback, useState,
-} from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAccountInfoSelector } from '../../../selectors';
 import { getTransferHistory } from '../../../actions/assets';
-import { BlockUpdater } from '../../block-subscriber';
 import SiteHeader from '../../components/site-header';
-import CustomTable from '../../components/tables/table1';
+import { TableLoader } from '../../components/TableLoader';
 import TransferHistoryItem from './transfer-history-item';
 
-export default function ScheduledTransactions() {
+export default function TransferHistory() {
   const dispatch = useDispatch();
-
   const { accountRS } = useSelector(getAccountInfoSelector);
 
-  const [transfers, setTransfers] = useState(null);
-  const [pagination, setPagination] = useState({
-    page: 1,
-    firstIndex: 0,
-    lastIndex: 15,
-  });
-
-  const getAssets = useCallback(async () => {
+  const getAssets = useCallback(async ({ firstIndex, lastIndex }) => {
     const newTransfers = await dispatch(getTransferHistory({ 
         account: accountRS,
-        firstIndex: pagination.firstIndex,
-        lastIndex: pagination.lastIndex
+        firstIndex,
+        lastIndex,
     }));
 
-    if (newTransfers) {
-      setTransfers(newTransfers.transfers);
-    }
-  }, [accountRS, pagination.firstIndex, pagination.lastIndex]);
-
-  const onPaginate = useCallback(page => () => {
-    setPagination({
-      page,
-      firstIndex: page * 15 - 15,
-      lastIndex: page * 15,
-    });
-  }, [setPagination]);
-
-  const listener = useCallback(() => getAssets(), [getAssets]);
-
-  useEffect(() => {
-    getAssets();
-  }, [getAssets]);
-
-  useEffect(() => {
-    BlockUpdater.on('data', listener);
-    return () => BlockUpdater.removeListener('data', listener);
-  }, [listener]);
+    return newTransfers?.transfers ?? [];
+  }, [dispatch, accountRS]);
 
   return (
     <div className="page-content">
       <SiteHeader pageTitle="Transfer History" />
       <div className="page-body container-fluid">
-        <CustomTable
-          header={[
+        <TableLoader
+          headersList={[
             {
               name: 'Transaction',
               alignRight: false,
@@ -85,13 +53,8 @@ export default function ScheduledTransactions() {
           ]}
           emptyMessage="No asset transfer history available."
           className="mb-3"
-          page={pagination.page}
           TableRowComponent={TransferHistoryItem}
-          tableData={transfers}
-          isPaginate
-          previousHendler={onPaginate(pagination.page - 1)}
-          nextHendler={onPaginate(pagination.page + 1)}
-          itemsPerPage={15}
+          dataLoaderCallback={getAssets}
         />
       </div>
     </div>
