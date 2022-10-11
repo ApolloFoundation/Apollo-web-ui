a51ebf4fe08eb9138443308daaa3ebbb4f27fedc
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-10-11 16:06:14 +0400

    fix styles error

--- src/containers/account/exchange/dashboard/exchange-header/index.js
+++ src/containers/account/exchange/dashboard/exchange-header/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import classNames from "classnames";
 import {NotificationManager} from "react-notifications";
-
 import BtcIcon from '../../../../../assets/BTC.png';
 import EthIcon from '../../../../../assets/ETH.png';
 import PaxIcon from '../../../../../assets/PAX.png';
+
 const currencyIcons = {btc: BtcIcon, eth: EthIcon, pax: PaxIcon};
 
 const ExchangeHeader = ({currencies, currentCurrency, switchCurrency, wallet, handleLoginModal}) => (
-    <div className={'btn-light-box'}>
+    <div className='btn-light-box'>
         {currencies && currencies.map(item => {
             const currency = item.toUpperCase();
             return (
                 <button
-                    type={'button'}
+                    key={item}
+                    type='button'
                     className={classNames({
                         "btn-light primary d-inline-block ml-3 mt-0" : true,
                         "active" : item === currentCurrency.currency,
