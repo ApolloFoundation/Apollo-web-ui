55ec27cba1769101ef321f146226004ffb3c38cd
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-12-22 18:03:36 +0400

    refactor composeMessage modal

--- src/containers/modals/messenger/compose-message/form.js
+++ src/containers/modals/messenger/compose-message/form.js
@@ -1,40 +1,25 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import AccountRSFormInput from '../../../components/form-components/account-rs1'
+import CustomTextArea from '../../../components/form-components/text-area1'
+import CheckboxFormInput from '../../../components/check-button-input';
 
-import AccountRSFormInput from '../../../components/form-components/account-rs'
-import CustomTextArea from '../../../components/form-components/text-area'
-import {CheckboxFormInput} from '../../../components/form-components/check-button-input';
-
-const ComposeMessageForm = ({setValue, getValue, modalData, getFormState, values}) => (
+const ComposeMessageForm = () => (
     <>
         <AccountRSFormInput
-            setValue={setValue}
-            label={'Recipient'}
-            field={'recipient'}
-            value={getValue('recipient') || ''}
-            defaultValue={(modalData && modalData.recipient) ? modalData.recipient : ''}
+            label='Recipient'
+            name='recipient'
         />
         <CustomTextArea
-            setVaue={setValue}
-            label={'Message'}
-            field={'message'}
-            placeholder={'Message'}
+            label='Message'
+            name='message'
+            placeholder='Message'
         />
         <CheckboxFormInput
-            setVaue={setValue}
-            checkboxes={[
-                {
-                    field: 'messageToEncrypt',
-                    label: 'Encrypt Message',
-                    defaultValue: true
-                }
-            ]}
+            name='messageToEncrypt'
+            label='Encrypt Message'
+            id="messageToEncryptCheckbox"
         />
     </>
 )
 
-const mapStateToProps = state => ({
-    modalData: state.modals.modalData
-})
-
-export default connect(mapStateToProps)(ComposeMessageForm);
+export default ComposeMessageForm;
