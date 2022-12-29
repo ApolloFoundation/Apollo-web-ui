9c6ae29b9f2ddf8acbd5cdcb0dbfe459652db83d
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2022-12-29 17:15:27 +0400

    refactor peers modals

--- src/containers/modals/peer/connect-peer.js
+++ src/containers/modals/peer/connect-peer.js
@@ -4,126 +4,66 @@
  ******************************************************************************/
 
 
-import React from 'react';
-import {connect} from 'react-redux';
-
-import {Form, Text} from 'react-form';
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {NotificationManager} from "react-notifications";
 import submitForm from "../../../helpers/forms/forms";
-import InputForm from '../../components/input-form';
-import classNames from "classnames";
+import TextualInput from '../../components/form-components/textual-input/textual-input1';
+import ModalBody from '../../components/modals/modal-body';
+import { getModalDataSelector } from '../../../selectors';
 
-class ConnectPeer extends React.Component {
-    state = {
-        isPending: false,
-    };
+const ConnectPeer = ({ closeModal }) => {
+    const dispatch = useDispatch();
+    const [isPending, setIsPending] = useState(false);
+    const modalData = useSelector(getModalDataSelector);
 
-    handleFormSubmit = async (values) => {
-        if (!this.state.isPending) {
-            this.setState({isPending: true});
-            const toSend = {
-                adminPassword: values.adminPassword,
-                peer: this.props.modalData || values.peer,
-            };
-            const res = await this.props.submitForm(toSend, "addPeer");
-            if (res.errorCode) {
-                NotificationManager.error(res.errorDescription, 'Error', 5000)
-            } else {
-                NotificationManager.success('Peer has been conected!', null, 5000);
-                this.props.closeModal();
-            }
-            this.setState({isPending: false});
+    const handleFormSubmit = useCallback(async (values) => {
+        setIsPending(true);
+        const toSend = {
+            adminPassword: values.adminPassword,
+            peer: modalData || values.peer,
+        };
+        const res = await dispatch(submitForm.submitForm(toSend, "addPeer"));
+        if (res.errorCode) {
+            NotificationManager.error(res.errorDescription, 'Error', 5000)
+        } else {
+            NotificationManager.success('Peer has been conected!', null, 5000);
+            closeModal();
         }
-    };
+        setIsPending(false);
+    }, [dispatch, closeModal, modalData]);
 
-    render() {
-        return (
-            <div className="modal-box">
-                <Form
-                    onSubmit={(values) => this.handleFormSubmit(values)}
-                    render={({submitForm, setValue}) => (
-                        <form className="modal-form" onSubmit={submitForm}>
-                            <div className="form-group-app">
-                                <button type="button" onClick={() => this.props.closeModal()} className="exit"><i
-                                    className="zmdi zmdi-close"/></button>
-
-                                <div className="form-title">
-                                    <p>{this.props.modalData ? 'Connect Peer' : 'Add Peer'}</p>
-                                </div>
-                                <div className="form-group mb-15">
-                                    <label>
-                                        Address
-                                    </label>
-                                    <div>
-                                        {this.props.modalData ?
-                                            <div className="input-wrapper">
-                                                {this.props.modalData}
-                                            </div>
-                                            :
-                                            <InputForm
-                                                field="peer"
-                                                placeholder="Peer Address"
-                                                setValue={setValue}/>
-                                        }
-                                    </div>
-                                </div>
-                                <div className="form-group mb-15">
-                                    <label>
-                                        Admin Password
-                                    </label>
-                                    <div>
-                                        <Text className="form-control"
-                                              field="adminPassword"
-                                              placeholder="Admin Password"
-                                              type={'password'}/>
-                                    </div>
-                                </div>
-                                <div className="btn-box right-conner align-right form-footer">
-                                    <button
-                                        type={'button'}
-                                        onClick={() => this.props.closeModal()}
-                                        className="btn btn-default mr-3"
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        type="submit"
-                                        name={'closeModal'}
-                                        className={classNames({
-                                            "btn btn-green submit-button": true,
-                                            "loading btn-green-disabled": this.state.isPending,
-                                        })}
-                                    >
-                                        <div className="button-loader">
-                                            <div className="ball-pulse">
-                                                <div/>
-                                                <div/>
-                                                <div/>
-                                            </div>
-                                        </div>
-                                        <span className={'button-text'}>Send</span>
-                                    </button>
-
-                                </div>
-                            </div>
-                        </form>
-                    )}
-                >
-
-                </Form>
-            </div>
-        );
-    }
+    return (
+        <ModalBody
+            modalTitle={modalData ? 'Connect Peer' : 'Add Peer'}
+            closeModal={closeModal}
+            handleFormSubmit={handleFormSubmit}
+            submitButtonName='Send'
+            idGroup='peers-modal-'
+            isDisableSecretPhrase
+            isPending={isPending}
+        >
+            <>
+                {modalData ?
+                    <div className="input-wrapper mb-15">
+                        {modalData}
+                    </div>
+                    :
+                    <TextualInput
+                        label="Address"
+                        name="peer"
+                        placeholder="Peer Address"
+                    />
+                }
+            </>
+            <TextualInput
+                label="Admin Password"
+                name="adminPassword"
+                placeholder="Peer Address"
+                type='password'
+            />
+        </ModalBody>
+    );
 }
 
-const mapStateToProps = state => ({
-    modalData: state.modals.modalData,
-    publicKey: state.account.publicKey
-
-});
-
-const mapDispatchToProps = dispatch => ({
-    submitForm: (data, requestType) => dispatch(submitForm.submitForm(data, requestType)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ConnectPeer);
+export default ConnectPeer;
