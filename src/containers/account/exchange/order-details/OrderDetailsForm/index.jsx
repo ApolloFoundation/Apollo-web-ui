557601e5b7caac9d8564b8d34e249769c8f0a9df
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-01-05 16:16:29 +0400

    refactor order details form

---
+++ src/containers/account/exchange/order-details/OrderDetailsForm/index.jsx
@@ -0,0 +1,101 @@
+import React, { useCallback } from 'react';
+import { Formik, Form } from "formik";
+import TextualInputComponent from "../../../../components/form-components/textual-input/textual-input1";
+import SimpleProgressBar from '../../../../components/simple-progress-bar/simple-progress-bar';
+import ContractStatusItem from '../contract-status-item';
+
+export const OrderDetailsForm = ({
+  selectedContractStatus,
+  allContractStatus,
+  account,
+  orderInfo,
+  isShowSelectedContractStatus,
+  isShowingContractHistory,
+  currentContractOrder,
+}) => {
+  const renderMoreDetails = useCallback(type => {
+    if (type) {
+      return (
+        <ContractStatusItem isContractHistory account={account} contracts={allContractStatus} />
+      );
+    }
+    return (
+      <ContractStatusItem
+        account={account}
+        contracts={selectedContractStatus}
+        label="Contract (Status) details"
+      />
+    );
+  }, [account, allContractStatus, selectedContractStatus]);
+
+  return (
+    <Formik initialValues={{
+      current: `${account.ticker}/${orderInfo.type.toUpperCase()}`,
+      typeName: orderInfo.typeName,
+      pairRate: orderInfo.pairRate,
+      offerAmount: orderInfo.offerAmount,
+      total: orderInfo.total,
+      status: orderInfo.statusName,
+    }}>
+      <Form className="modal-form">
+        <div className="form-group-app">
+          {!isShowingContractHistory
+            ? (
+              <>
+                {isShowSelectedContractStatus && (
+                  <SimpleProgressBar
+                    contractOrder={currentContractOrder}
+                    blockTime={account.timestamp}
+                    status={orderInfo.statusName}
+                  />
+                )}
+                <TextualInputComponent
+                  name="current"
+                  label="Pair Name"
+                  defaultValue={`${account.ticker}/${orderInfo.type.toUpperCase()}`}
+                  disabled
+                  placeholder="Pair Name"
+                />
+                <TextualInputComponent
+                  name="typeName"
+                  label="Type"
+                  defaultValue={orderInfo.typeName}
+                  disabled
+                  placeholder="Type"
+                />
+                <TextualInputComponent
+                  name="pairRate"
+                  label="Price"
+                  defaultValue={orderInfo.pairRate}
+                  disabled
+                  placeholder="Price"
+                />
+                <TextualInputComponent
+                  name="offerAmount"
+                  label="Amount"
+                  defaultValue={orderInfo.offerAmount}
+                  disabled
+                  placeholder="Amount"
+                />
+                <TextualInputComponent
+                  name="total"
+                  label="Total"
+                  disabled
+                  defaultValue={orderInfo.total}
+                  placeholder="Total"
+                />
+                <TextualInputComponent
+                  name="status"
+                  label="Status"
+                  defaultValue={orderInfo.statusName}
+                  disabled
+                  placeholder="Status"
+                />
+                {isShowSelectedContractStatus && renderMoreDetails()}
+              </>
+            ) : (!!(allContractStatus && allContractStatus.length) && renderMoreDetails('history'))}
+          </div>
+      </Form>
+    </Formik>
+  );
+}
--- src/containers/account/exchange/order-details/index.jsx
+++ src/containers/account/exchange/order-details/index.jsx
@@ -1,7 +1,6 @@
 import React, {
   useState, useEffect, useCallback,
 } from 'react';
-import { Form } from 'react-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouteMatch, useHistory } from 'react-router-dom';
 import { NotificationManager } from 'react-notifications';
@@ -12,12 +11,10 @@ import {
   getAllContractStatus, getContractStatus, getOrderById,
 } from '../../../../actions/wallet';
 import SiteHeader from '../../../components/site-header';
-import TextualInputComponent from '../../../components/form-components/textual-input';
-import SimpleProgressBar from '../../../components/simple-progress-bar/simple-progress-bar';
 import ContentLoader from '../../../components/content-loader';
 import Button from '../../../components/button';
-import ContractStatusItem from './contract-status-item';
 import InfoBox from '../../../components/info-box';
+import { OrderDetailsForm } from './OrderDetailsForm';
 
 export default function OrderDetails() {
   const dispatch = useDispatch();
@@ -32,21 +29,6 @@ export default function OrderDetails() {
 
   const match = useRouteMatch();
   const history = useHistory();
-
-  const renderMoreDetails = useCallback(type => {
-    if (type) {
-      return (
-        <ContractStatusItem isContractHistory account={account} contracts={allContractStatus} />
-      );
-    }
-    return (
-      <ContractStatusItem
-        account={account}
-        contracts={selectedContractStatus}
-        label="Contract (Status) details"
-      />
-    );
-  }, [account, allContractStatus, selectedContractStatus]);
 
   const handleBack = useCallback(() => {
     history.push({ pathname: '/order-history' });
@@ -133,73 +115,18 @@ export default function OrderDetails() {
                     <>
                       {!orderInfo.hasFrozenMoney && orderInfo.status === 0 && (
                       <InfoBox default>
-                        Order cant be matched, your deposit doesnt allow to freeze
+                        Order cant be matched, your deposit doesn't allow to freeze
                         funds.
                       </InfoBox>
                       )}
-                      <Form
-                        render={() => (
-                          <form className="modal-form">
-                            <div className="form-group-app">
-                              {!isShowingContractHistory
-                                ? (
-                                  <>
-                                    {isShowSelectedContractStatus && (
-                                      <SimpleProgressBar
-                                        contractOrder={currentContractOrder}
-                                        blockTime={account.timestamp}
-                                        status={orderInfo.statusName}
-                                      />
-                                    )}
-                                    <TextualInputComponent
-                                      field="current"
-                                      label="Pair Name"
-                                      defaultValue={`${account.ticker}/${orderInfo.type.toUpperCase()}`}
-                                      disabled
-                                      placeholder="Pair Name"
-                                    />
-                                    <TextualInputComponent
-                                      field="typeName"
-                                      label="Type"
-                                      defaultValue={orderInfo.typeName}
-                                      disabled
-                                      placeholder="Type"
-                                    />
-                                    <TextualInputComponent
-                                      field="pairRate"
-                                      label="Price"
-                                      defaultValue={orderInfo.pairRate}
-                                      disabled
-                                      placeholder="Price"
-                                    />
-                                    <TextualInputComponent
-                                      field="offerAmount"
-                                      label="Amount"
-                                      defaultValue={orderInfo.offerAmount}
-                                      disabled
-                                      placeholder="Amount"
-                                    />
-                                    <TextualInputComponent
-                                      field="total"
-                                      label="Total"
-                                      disabled
-                                      defaultValue={orderInfo.total}
-                                      placeholder="Total"
-                                    />
-                                    <TextualInputComponent
-                                      field="status"
-                                      label="Status"
-                                      defaultValue={orderInfo.statusName}
-                                      disabled
-                                      placeholder="Status"
-                                    />
-                                    {isShowSelectedContractStatus && renderMoreDetails()}
-                                  </>
-                                ) : (!!(allContractStatus && allContractStatus.length)
-                                  && renderMoreDetails('history'))}
-                            </div>
-                          </form>
-                        )}
+                      <OrderDetailsForm 
+                        account={account}
+                        allContractStatus={allContractStatus}
+                        selectedContractStatus={selectedContractStatus}
+                        isShowSelectedContractStatus={isShowSelectedContractStatus}
+                        orderInfo={orderInfo}
+                        isShowingContractHistory={isShowingContractHistory}
+                        currentContractOrder={currentContractOrder}
                       />
                     </>
                   ) : (
