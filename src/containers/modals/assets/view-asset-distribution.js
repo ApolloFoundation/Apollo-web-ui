cd65d3206fbe0ebdeba4dbefb083382c77b33b6a
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-01-12 17:46:30 +0400

    refactor asset-distribution modal

--- src/containers/modals/assets/view-asset-distribution.js
+++ src/containers/modals/assets/view-asset-distribution.js
@@ -9,7 +9,8 @@ import {useDispatch, useSelector} from 'react-redux';
 import {setBodyModalParamsAction} from '../../../modules/modals';
 import {getAccountAssetsAction} from "../../../actions/assets";
 import { useDataLoader } from '../../../hooks/useDataLoader';
-import { getModalDataSelector } from 'selectors';
+import { getModalDataSelector } from '../../../selectors';
+import ModalBody from '../../components/modals/modal-body';
 
 const AssetDistribution = (props) => {
     const dispatch = useDispatch();
@@ -20,7 +21,7 @@ const AssetDistribution = (props) => {
             requestType: 'getAssetAccounts',
             asset: modalData.asset
         }));
-        return res.accountAssets ?? [];
+        return res?.accountAssets ?? [];
     }, [dispatch, modalData.asset]);
 
     const handleOpenModal = (account) => () => dispatch(setBodyModalParamsAction('INFO_ACCOUNT', account));
@@ -28,54 +29,45 @@ const AssetDistribution = (props) => {
     const { data } = useDataLoader(handleLoadData);
 
     return (
-        <div className="modal-box">
-            <div className="modal-form">
-                <div className="form-group-app">
-                    <button type="button" onClick={props.closeModal} className="exit">
-                        <i className="zmdi zmdi-close"/>
-                    </button>
-                    <div className="form-title">
-                        <p>Asset Distribution</p>
-                    </div>
-                    <div className="transaction-table no-min-height">
-                        <div className="transaction-table-body transparent padding-vertical-padding">
+        <ModalBody
+            modalTitle="Asset Distribution"
+            closeModal={props.closeModal}
+            isDisableFormFooter
+        >
+                <div className="transaction-table no-min-height">
+                    <div className="transaction-table-body transparent padding-vertical-padding">
 
-                            <table>
-                                <thead>
-                                <tr>
-                                    <td>Account</td>
-                                    <td>Quantity</td>
-                                    <td className="align-right">Percentage</td>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {
-                                    data &&
-                                    data.length &&
-                                    data.map((el) => (
-                                        <tr key={el.accountRS}>
-                                            <td
+                        <table>
+                            <thead>
+                            <tr>
+                                <td>Account</td>
+                                <td>Quantity</td>
+                                <td className="align-right">Percentage</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                data &&
+                                data.length &&
+                                data.map((el) => (
+                                    <tr key={el.accountRS}>
+                                        <td className='blue-link-text'>
+                                            <span
                                                 className='blue-link-text'
-                                            >
-                                                <span
-                                                    className='blue-link-text'
-                                                    onClick={handleOpenModal(el.account)}>
-                                                    {el.accountRS}
-                                                </span>
-                                            </td>
-
-                                            <td>{el.quantityATU / Math.pow(10, modalData.decimals)}</td>
-                                            <td className="align-right">{(el.quantityATU / modalData.totalAvailable) * 100} %</td>
-                                        </tr>
-                                    ))
-                                }
-                                </tbody>
-                            </table>
-                        </div>
+                                                onClick={handleOpenModal(el.account)}>
+                                                {el.accountRS}
+                                            </span>
+                                        </td>
+                                        <td>{el.quantityATU / Math.pow(10, modalData.decimals)}</td>
+                                        <td className="align-right">{(el.quantityATU / modalData.totalAvailable) * 100} %</td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </div>
-        </div>
+        </ModalBody>
     );
 }
 
