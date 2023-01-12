bdeff97789c3dbced9156e094aebeaa3b72fd2e3
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-01-12 16:54:30 +0400

    refactor account-info

--- src/containers/modals/account/account-info.js
+++ src/containers/modals/account/account-info.js
@@ -6,16 +6,14 @@
 
 import React, { useCallback, useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import classNames from "classnames";
 import {NotificationManager} from "react-notifications";
-import {openPrevModal, setBodyModalParamsAction} from '../../../modules/modals';
+import {setBodyModalParamsAction} from '../../../modules/modals';
 import submitForm from "../../../helpers/forms/forms";
-import ModalFooter from '../../components/modal-footer';
-import BackForm from '../modal-form/modal-form-container';
 import CustomInput from '../../components/custom-input/CustomInputWithFormik';
 import CustomTextArea from '../../components/form-components/TextArea/TextAreaWithFormik';
-import { getAccountInfoSelector, getModalHistorySelector } from '../../../selectors';
+import { getAccountInfoSelector } from '../../../selectors';
 import { FeeWrapper } from '../../components/form-components/FeeWrapper';
+import ModalBody from '../../components/modals/modal-body';
 
 const  AccountInfo = (props) => {
     const dispatch = useDispatch();
@@ -24,7 +22,6 @@ const  AccountInfo = (props) => {
     });
 
     const accountInfo = useSelector(getAccountInfoSelector);
-    const modalsHistory = useSelector(getModalHistorySelector);
 
     const handleFormSubmit = useCallback(async (values) => {
         if (!state.isPending) {
@@ -42,78 +39,35 @@ const  AccountInfo = (props) => {
         }
     }, [dispatch, state.isPending]);
 
-    const handleOpenPrevModal = () => dispatch(openPrevModal());
-
     return (
-        <div className="modal-box">
-            <BackForm
-                nameModal={props.nameModal}
-                onSubmit={handleFormSubmit}
-                initialValues={{ 
-                    feeATM: 1,
-                    isCustomFee: true,
-                    name: accountInfo.name,
-                    description: accountInfo.description
-                }}
-            >
-                <div className="form-group-app">
-                    <button type="button" onClick={props.closeModal} className="exit">
-                        <i className="zmdi zmdi-close"/>
-                    </button>
-                    <div className="form-title">
-                        {modalsHistory.length > 1 && (
-                            <div className="backMy" onClick={handleOpenPrevModal}></div>
-                        )}
-                        <p>Set Account Info</p>
-                    </div>
-                    <div className="form-group mb-15">
-                        <CustomInput
-                            label="Name"
-                            placeholder="Your name"
-                            name="name"
-                        />
-                    </div>
-                    <div className="form-group mb-15">
-                        <CustomTextArea
-                            label="Description"
-                            placeholder="Description"
-                            name="message"
-                            cols="6"
-                            rows="10"
-                        />
-                    </div>
-                    <FeeWrapper name='feeATM' />
-
-                    <ModalFooter />
-
-                    <div className="btn-box right-conner align-right form-footer">
-                        <button
-                            type='button'
-                            onClick={props.closeModal}
-                            className="btn btn-default mr-3"
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            type="submit"
-                            name='closeModal'
-                            className={classNames("btn btn-green submit-button", {
-                                "loading btn-green-disabled": state.isPending,
-                            })}
-                        >
-                            <div className="button-loader">
-                                <div className="ball-pulse">
-                                    <div/>
-                                    <div/>
-                                    <div/>
-                                </div>
-                            </div>
-                            <span className='button-text'>Update Account Info</span>
-                        </button>
-                    </div>
-                </div>
-            </BackForm>
-        </div>
+        <ModalBody
+            modalTitle="Set Account Info"
+            submitButtonName="Update Account Info"
+            nameModal={props.nameModal}
+            handleFormSubmit={handleFormSubmit}
+            initialValues={{ 
+                feeATM: 1,
+                isCustomFee: false,
+                name: accountInfo.name,
+                description: accountInfo.description,
+            }}
+            closeModal={props.closeModal}
+            isPending={state.isPending}
+        >
+            <CustomInput
+                label="Name"
+                placeholder="Your name"
+                name="name"
+            />
+            <CustomTextArea
+                label="Description"
+                placeholder="Description"
+                name="message"
+                cols="6"
+                rows="10"
+            />
+            <FeeWrapper name='feeATM' />
+        </ModalBody>
     );
 }
 
