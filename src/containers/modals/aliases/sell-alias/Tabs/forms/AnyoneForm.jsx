a57761ece4cace77ec63402812b2412b9a77e4fc
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-01-26 13:57:29 +0400

    remove message checkboxes from Sell Alias modal (Anyone part)

--- src/containers/modals/aliases/sell-alias/Tabs/forms/AnyoneForm.jsx
+++ src/containers/modals/aliases/sell-alias/Tabs/forms/AnyoneForm.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { useFormikContext } from 'formik';
 import TextualInputComponent from 'containers/components/form-components/TextualInput';
 import NumericInputComponent from 'containers/components/form-components/NumericInput';
-import CheckboxFormInput from 'containers/components/check-button-input/CheckboxWithFormik';
-import CustomTextArea from 'containers/components/form-components/TextArea/TextAreaWithFormik';
 
-export const AnyoneForm = ({ alias }) => {
-  const { values } = useFormikContext();
-  return (
+export const AnyoneForm = ({ alias }) => (
     <>
       {
           alias && 
@@ -16,37 +11,10 @@ export const AnyoneForm = ({ alias }) => {
               text={alias.aliasName}
           />
       }
-
       <NumericInputComponent
           label='Price'
           name='priceATM'
           placeholder='Price'
       />
-      <CheckboxFormInput
-          name='add_message'
-          label='Add a message?'
-          id='add_message-anyone-account-'
-      />
-      {
-          values.add_message && 
-          <>
-              <CustomTextArea
-                  label='Message'
-                  name='message'
-                  placeholder='Message'
-              />
-              <CheckboxFormInput 
-                  label='Encrypt Message'
-                  name='encrypt_message'
-                  id="encrypt_message-to-anyone"
-              />
-              <CheckboxFormInput
-                  label='Message is Never Deleted'
-                  name='permanent_message'
-                  id="permanent_message-to-anyone"
-              />
-          </>
-      }
     </>
-  );
-}
+);
