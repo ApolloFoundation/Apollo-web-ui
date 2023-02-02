dfda5f42adbe6235bc7a35405c85393cb2750e9e
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-02-02 15:04:09 +0400

    make reconfiguration and small optimization for login page part for login

--- src/containers/account/login/login-modal/login-accordions/standart-wallet/index.jsx
+++ src/containers/account/login/login-modal/login-accordions/standart-wallet/index.jsx
@@ -1,43 +1,40 @@
 import React from 'react';
 import { Accordion } from 'containers/components/accordion';
 import { Tooltip } from 'containers/components/tooltip-login';
-import SecretPhraseForm from '../../forms/viaSecretPhraseForm';
+import SecretPhraseForm from './StandartForm';
 import { TooltipWrapper } from '../tooltipWrapper';
 
 import styles from './index.module.scss';
 
-export const StandartWallet = ({ active, onChange, id }) => {
+const tooltips = [
+  {
+    iconChecked: true,
+    text: 'The most secure Apollo Wallet',
+  },
+  {
+    iconChecked: true,
+    text: 'You can log in to this wallet using your Account ID or secret phrase',
+  },
+  {
+    iconChecked: false,
+    text: '2FA is available only on the device where it was enabled',
+  },
+];
 
-  const tooltips = [
-    {
-      iconChecked: true,
-      text: 'The most secure Apollo Wallet',
-    },
-    {
-      iconChecked: true,
-      text: 'You can log in to this wallet using your Account ID or secret phrase',
-    },
-    {
-      iconChecked: false,
-      text: '2FA is available only on the device where it was enabled',
-    },
-  ];
-
-  return (
-    <Accordion
-      id={id}
-      onSelectItem={onChange}
-      currentState={active === id}
-      header={
-        <div>
-          <span className={styles.standartWalletText}>standard wallet</span>
-          <Tooltip>
-            <TooltipWrapper list={tooltips} />
-          </Tooltip>
-        </div>
-      }
-    >
-      <SecretPhraseForm /> 
-    </Accordion>
-  );
-}
+export const StandartWallet = ({ active, onChange, id }) => (
+  <Accordion
+    id={id}
+    onSelectItem={onChange}
+    currentState={active === id}
+    header={
+      <div>
+        <span className={styles.standartWalletText}>standard wallet</span>
+        <Tooltip>
+          <TooltipWrapper list={tooltips} />
+        </Tooltip>
+      </div>
+    }
+  >
+    <SecretPhraseForm /> 
+  </Accordion>
+);
