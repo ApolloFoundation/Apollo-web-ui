bcd0bf777cf0f67ac5110ffce2ad37b953129240
Roman Gavrylenko <roman.gavrylenko@firstbridge.io>
2023-04-20 19:02:47 +0400

    clean up

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {};
 
 // API Gateway
 config.api = {
-  server: 'https://apl-t2-2.testnet2.apollowallet.org',
+  server: '',
   transportUrl: 'ws://127.0.0.1:8888/',
   faucetUrl: 'https://wallet.test.apollowallet.org',
 };
